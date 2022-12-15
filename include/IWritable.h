f83ec33396800bd3e03bfce307ab153eb3b80b34
Alejandro Alfonso <alex.1993.24@gmail.com>
2022-12-15 15:54:23 +0100

    Fix IWritable.h format

--- include/IWritable.h
+++ include/IWritable.h
@@ -7,19 +7,19 @@ namespace futsim
 
 /**
  * @brief Interface for classes that can be saved and loaded from input/output streams.
-*/
+ */
 class IWritable
 {
 public:
 	/**
 	 * @brief Virtual destructor for base class.
-	*/
+	 */
 	virtual ~IWritable() = default;
 
 	/**
 	 * @brief Output class to stream.
 	 * @param aOutput Output stream.
-	*/
+	 */
 	virtual void SaveToTXT( std::ostream& aOutput ) const = 0;
 };
 
