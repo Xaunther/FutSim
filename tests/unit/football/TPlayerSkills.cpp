ebf03ae216822f1cde00bcb7b7db02911b4938ab
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-14 15:48:43 +0100

    Test member constructor

--- tests/unit/football/TPlayerSkills.cpp
+++ tests/unit/football/TPlayerSkills.cpp
@@ -1,7 +1,18 @@
 #include "ITest.h"
 
+#include "football/CPlayerSkills.h"
+
+#include <iostream>
+
+using namespace futsim::football;
+
 INITIALIZE_TEST( TPlayerSkills )
 
 void TPlayerSkills::DoRun() const
 {
+	//! Test member constructor
+	CheckException( []() { CPlayerSkills{ 0, 1, 1, 1, 1, 1, 1, 1 }; }, "The skill value must be greater than 0." );
+	CheckException( []() { CPlayerSkills{ 1, 0, 1, 1, 1, 1, 1, 1 }; }, "The skill value must be greater than 0." );
+	CheckException( []() { CPlayerSkills{ 1, 1, 0, 1, 1, 1, 1, 1 }; }, "The skill value must be greater than 0." );
+	CheckException( []() { CPlayerSkills{ 1, 1, 1, 0, 1, 1, 1, 1 }; }, "The skill value must be greater than 0." );
 }
