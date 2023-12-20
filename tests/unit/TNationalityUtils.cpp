157b0048353dac1cd38e532f26f3f5e2369696bc
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-20 15:16:02 +0100

    Add NationalityUtils test

---
+++ tests/unit/TNationalityUtils.cpp
@@ -0,0 +1,26 @@
+#include "ITest.h"
+
+#include "NationalityUtils.h"
+
+#include <iostream>
+
+using namespace futsim;
+
+INITIALIZE_TEST( TPlayerSkills )
+
+void TPlayerSkills::TestExceptions() const
+{
+}
+
+std::vector<std::string> TPlayerSkills::ObtainedResults() const noexcept
+{
+	std::vector<std::string> result;
+
+	return result;
+}
+
+std::vector<std::string> TPlayerSkills::ExpectedResults() const noexcept
+{
+	std::vector<std::string> result;
+	return result;
+}
