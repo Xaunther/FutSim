e8cbab37d3441bd68e4b240c24b9960aa12cb81d
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-20 12:36:38 +0100

    Add members

--- include/football/CPlayer.h
+++ include/football/CPlayer.h
@@ -2,14 +2,32 @@
 
 #include "IJsonable.h"
 
+#include "ENationality.h"
+#include "football/CPlayerSkills.h"
+
 namespace futsim
 {
 
 namespace football
 {
 
+/**
+ * @brief Class that defines a football player
+*/
 class CPlayer : public IJsonable
 {
+	//! First name.
+	std::string mFirstName;
+	//! Surnames.
+	std::string mSurnames;
+	//! Name by which the player is known.
+	std::string mKnownName;
+	//! Age.
+	unsigned short mAge;
+	//! Nationality (TBD)
+	E_NATIONALITY mNationality;
+	//! Skills
+	CPlayerSkills mPlayerSkills;
 };
 
 } // football namespace
