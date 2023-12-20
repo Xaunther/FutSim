af2d2f97d13f4902dc3dad33b42db5541a50e811
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-20 15:32:11 +0100

    Test NationalityUtils

--- tests/unit/TNationalityUtils.cpp
+++ tests/unit/TNationalityUtils.cpp
@@ -10,17 +10,31 @@ INITIALIZE_TEST( TPlayerSkills )
 
 void TPlayerSkills::TestExceptions() const
 {
+	//! Test ToNationality exception
+	CheckException( []() { ToNationality( "AAA" ); }, "No nationality matching the code 'AAA'." );
 }
 
 std::vector<std::string> TPlayerSkills::ObtainedResults() const noexcept
 {
-	std::vector<std::string> result;
-
+	std::vector<std::string> result{
+		ToString( E_NATIONALITY::AGO ),
+		ToString( E_NATIONALITY::CAF ),
+		ToString( E_NATIONALITY::ESP ),
+		ToString( ToNationality( "AGO" ) ),
+		ToString( ToNationality( "CAF" ) ),
+		ToString( ToNationality( "ESP" ) ),
+	};
 	return result;
 }
 
 std::vector<std::string> TPlayerSkills::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
+	std::vector<std::string> result{
+		"AGO",
+		"CAF",
+		"ESP"
+	};
+	result.reserve( result.size() * 2 );
+	result.insert( result.cend(), result.cbegin(), result.cend() );
 	return result;
 }
