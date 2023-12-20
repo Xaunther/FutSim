90a4419248ed5d38de7275541fba86e1e3c9d22d
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-20 17:01:59 +0100

    Test JSON constructor

--- tests/unit/football/TPlayer.cpp
+++ tests/unit/football/TPlayer.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 using namespace futsim::football;
+using namespace nlohmann;
 
 INITIALIZE_TEST( TPlayer )
 
@@ -15,6 +16,48 @@ void TPlayer::TestExceptions() const
 		"The name cannot be empty." );
 	CheckException( []() { CPlayer{ "Lionel", "", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ 1, 1, 1, 1, 1, 1, 1, 1 } }; },
 		"The surnames cannot be empty." );
+
+	//! Test JSON constructor
+	CheckException( []() { CPlayer{ json::parse( R"( {
+			"Player": {}
+		} )" )[ CPlayer::JSON_NAME ] }; }, "key 'First name' not found" );
+	CheckException( []() { CPlayer{ json::parse( R"( {
+			"Player": {
+				"First name": ""
+			}
+		} )" )[ CPlayer::JSON_NAME ] }; }, "The name cannot be empty." );
+	CheckException( []() { CPlayer{ json::parse( R"( {
+			"Player": {
+				"First name": "Lionel"
+			}
+		} )" )[ CPlayer::JSON_NAME ] }; }, "key 'Surnames' not found" );
+	CheckException( []() { CPlayer{ json::parse( R"( {
+			"Player": {
+				"First name": "Lionel",
+				"Surnames": ""
+			}
+		} )" )[ CPlayer::JSON_NAME ] }; }, "The surnames cannot be empty." );
+	CheckException( []() { CPlayer{ json::parse( R"( {
+			"Player": {
+				"First name": "Lionel",
+				"Surnames": "Messi"
+			}
+		} )" )[ CPlayer::JSON_NAME ] }; }, "key 'Age' not found" );
+	CheckException( []() { CPlayer{ json::parse( R"( {
+			"Player": {
+				"First name": "Lionel",
+				"Surnames": "Messi",
+				"Age": 35
+			}
+		} )" )[ CPlayer::JSON_NAME ] }; }, "key 'Nationality' not found" );
+	CheckException( []() { CPlayer{ json::parse( R"( {
+			"Player": {
+				"First name": "Lionel",
+				"Surnames": "Messi",
+				"Age": 35,
+				"Nationality": "ARG"
+			}
+		} )" )[ CPlayer::JSON_NAME ] }; }, "key 'Player skills' not found" );
 }
 
 std::vector<std::string> TPlayer::ObtainedResults() const noexcept
