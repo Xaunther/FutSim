33808ce81f24be91c4998bee2ba28f6387fdc804
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-20 16:04:36 +0100

    Add member constructor

--- include/football/CPlayer.h
+++ include/football/CPlayer.h
@@ -16,6 +16,28 @@ namespace football
 */
 class CPlayer : public IJsonable
 {
+public:
+	/**
+	 * @brief Member constructor.
+	 * @param aFirstName \ref mFirstName
+	 * @param aSurnames \ref mSurnames
+	 * @param aKnownName \ref mKnownName
+	 * @param aAge \ref mAge
+	 * @param aNationality \ref mNationality
+	 * @param aPlayerSkills \ref mPlayerSkills
+	 * @pre The first name string cannot be empty.
+	 * @pre The surnames string cannot be empty.
+	 * @details If the passed known name is empty, it will default to the surnames.
+	*/
+	explicit CPlayer(
+		const std::string_view aFirstName,
+		const std::string_view aSurnames,
+		const std::string_view aKnownName,
+		const unsigned short& aAge,
+		const E_NATIONALITY& aNationality,
+		const CPlayerSkills& aPlayerSkills );
+
+private:
 	//! First name.
 	std::string mFirstName;
 	//! Surnames.
--- src/football/CPlayer.cpp
+++ src/football/CPlayer.cpp
@@ -1,1 +1,56 @@
 #include "football/CPlayer.h"
+
+#include "ExceptionUtils.h"
+
+namespace futsim
+{
+
+namespace football
+{
+
+namespace
+{
+
+/**
+ * @brief Checks correctness of the ability
+ * @param aSkill Skill value.
+ * @param aNameString String with the name label to add to the error message.
+*/
+const std::string_view CheckName( const std::string_view aName, const std::string_view aNameString );
+
+} // anonymous namespace
+
+CPlayer::CPlayer(
+	const std::string_view aFirstName,
+	const std::string_view aSurnames,
+	const std::string_view aKnownName,
+	const unsigned short& aAge,
+	const E_NATIONALITY& aNationality,
+	const CPlayerSkills& aPlayerSkills
+) try :
+	mFirstName( CheckName( aFirstName, "name" ) ),
+	mSurnames( CheckName( aSurnames, "surnames" ) ),
+	mKnownName( aKnownName.empty() ? mSurnames : CheckName( aKnownName, "known name" ) ),
+	mAge( aAge ),
+	mNationality( aNationality ),
+	mPlayerSkills( aPlayerSkills )
+{
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the player." )
+
+namespace
+{
+
+const std::string_view CheckName( const std::string_view aName, const std::string_view aNameString ) try
+{
+	if( aName.empty() )
+		throw std::invalid_argument{ "The " + std::string( aNameString.data() ) + " cannot be empty." };
+	return aName;
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error checking the " << aNameString << "." )
+
+} // anonymous namespace
+
+} // football namespace
+
+}  // futsim namespace
