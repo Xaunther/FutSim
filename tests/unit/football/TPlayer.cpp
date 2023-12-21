1063b07595f72c69e2e447273e2dd5059dd92f20
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-21 10:10:04 +0100

    Test CPlayer output

--- tests/unit/football/TPlayer.cpp
+++ tests/unit/football/TPlayer.cpp
@@ -2,6 +2,8 @@
 
 #include "football/CPlayer.h"
 
+#include "NationalityUtils.h"
+
 #include <iostream>
 
 using namespace futsim::football;
@@ -63,11 +65,134 @@ void TPlayer::TestExceptions() const
 std::vector<std::string> TPlayer::ObtainedResults() const noexcept
 {
 	std::vector<std::string> result;
+
+	for( const auto& player : {
+		CPlayer{ "Lionel", "Messi", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ 1, 1, 1, 99, 0, 0, 0, 0 } },
+		CPlayer{ "Ansu", "Fati", "Ansu Fati", 20, futsim::E_NATIONALITY::ESP, CPlayerSkills{ 1, 1, 1, 80, 0, 0, 0, 0 } },
+		CPlayer{ json::parse( R"( {
+			"Player": {
+				"Surnames": "Messi",
+				"First name": "Lionel",
+				"Age": 35,
+				"Nationality": "ARG",
+				"Player skills": {
+					"GK skill": 1,
+					"DF skill": 1,
+					"MF skill": 1,
+					"FW skill": 99,
+					"GK experience": 0,
+					"MF experience": 0,
+					"DF experience": 0,
+					"FW experience": 0
+				}
+			}
+		} )" )[ CPlayer::JSON_NAME ] },
+		CPlayer{ json::parse( R"( {
+			"Player": {
+				"First name": "Ansu",
+				"Surnames": "Fati",
+				"Known name": "Ansu Fati",
+				"Age": 20,
+				"Nationality": "ESP",
+				"Player skills": {
+					"GK skill": 1,
+					"DF skill": 1,
+					"MF skill": 1,
+					"FW skill": 80,
+					"GK experience": 0,
+					"MF experience": 0,
+					"DF experience": 0,
+					"FW experience": 0
+				}
+			}
+		} )" )[ CPlayer::JSON_NAME ] } } )
+	{
+		result.push_back( std::string{ CPlayer::JSON_FIRST_NAME } + ": " + std::string{ player.GetFirstName() } );
+		result.push_back( std::string{ CPlayer::JSON_SURNAMES } + ": " + std::string{ player.GetSurnames() } );
+		result.push_back( std::string{ CPlayer::JSON_KNOWN_NAME } + ": " + std::string{ player.GetKnownName() } );
+		result.push_back( std::string{ CPlayer::JSON_AGE } + ": " + std::to_string( player.GetAge() ) );
+		result.push_back( std::string{ CPlayer::JSON_NATIONALITY } + ": " + futsim::ToString( player.GetNationality() ) );
+		result.push_back( std::string{ CPlayerSkills::JSON_NAME } + ": " + player.GetPlayerSkills().ToJSON().dump( 1, '\t' ) );
+		futsim::IJsonableTypes::json outputJSON;
+		outputJSON[ CPlayer::JSON_NAME ] = player.ToJSON();
+		result.push_back( outputJSON.dump( 1, '\t' ) );
+	}
+
 	return result;
 }
 
 std::vector<std::string> TPlayer::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
+	std::vector<std::string> result{
+		"First name: Lionel",
+		"Surnames: Messi",
+		"Known name: Messi",
+		"Age: 35",
+		"Nationality: ARG",
+		"Player skills: {\n"
+		"	\"GK skill\": 1,\n"
+		"	\"DF skill\": 1,\n"
+		"	\"MF skill\": 1,\n"
+		"	\"FW skill\": 99,\n"
+		"	\"GK experience\": 0,\n"
+		"	\"DF experience\": 0,\n"
+		"	\"MF experience\": 0,\n"
+		"	\"FW experience\": 0\n"
+		"}",
+		"{\n"
+		"	\"Player\": {\n"
+		"		\"First name\": \"Lionel\",\n"
+		"		\"Surnames\": \"Messi\",\n"
+		"		\"Age\": 35,\n"
+		"		\"Nationality\": \"ARG\",\n"
+		"		\"Player skills\": {\n"
+		"			\"GK skill\": 1,\n"
+		"			\"DF skill\": 1,\n"
+		"			\"MF skill\": 1,\n"
+		"			\"FW skill\": 99,\n"
+		"			\"GK experience\": 0,\n"
+		"			\"DF experience\": 0,\n"
+		"			\"MF experience\": 0,\n"
+		"			\"FW experience\": 0\n"
+		"		}\n"
+		"	}\n"
+		"}",
+		"First name: Ansu",
+		"Surnames: Fati",
+		"Known name: Ansu Fati",
+		"Age: 20",
+		"Nationality: ESP",
+		"Player skills: {\n"
+		"	\"GK skill\": 1,\n"
+		"	\"DF skill\": 1,\n"
+		"	\"MF skill\": 1,\n"
+		"	\"FW skill\": 80,\n"
+		"	\"GK experience\": 0,\n"
+		"	\"DF experience\": 0,\n"
+		"	\"MF experience\": 0,\n"
+		"	\"FW experience\": 0\n"
+		"}",
+		"{\n"
+		"	\"Player\": {\n"
+		"		\"First name\": \"Ansu\",\n"
+		"		\"Surnames\": \"Fati\",\n"
+		"		\"Known name\": \"Ansu Fati\",\n"
+		"		\"Age\": 20,\n"
+		"		\"Nationality\": \"ESP\",\n"
+		"		\"Player skills\": {\n"
+		"			\"GK skill\": 1,\n"
+		"			\"DF skill\": 1,\n"
+		"			\"MF skill\": 1,\n"
+		"			\"FW skill\": 80,\n"
+		"			\"GK experience\": 0,\n"
+		"			\"DF experience\": 0,\n"
+		"			\"MF experience\": 0,\n"
+		"			\"FW experience\": 0\n"
+		"		}\n"
+		"	}\n"
+		"}"
+	};
+	result.reserve( 2 * result.size() );
+	result.insert( result.cend(), result.cbegin(), result.cend() );
 	return result;
 }
