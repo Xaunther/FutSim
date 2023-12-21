2dab4678894cd62f409802503ffcdfecf9a1851f
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-21 17:24:27 +0100

    Assert json template type

--- include/JsonUtils.h
+++ include/JsonUtils.h
@@ -5,6 +5,14 @@
 namespace futsim
 {
 
+//! Checks whether the given type is a supported json type.
+template<class T> struct is_json : std::bool_constant<
+	std::is_same_v<std::remove_const_t<std::remove_reference_t<T>>, nlohmann::ordered_json> ||
+	std::is_same_v<std::remove_const_t<std::remove_reference_t<T>>, nlohmann::json>> {};
+
+//! Helper value template.
+template<class T> inline constexpr bool is_json_v = is_json<T>::value;
+
 /**
  * @brief Helper function to get a value from a certain key in a JSON array.
  * @param aJSON JSON object.
@@ -26,12 +34,14 @@ T ValueFromOptionalJSONKey( const auto& aJSON, const std::string_view aKeyName, 
 template<typename T>
 T ValueFromRequiredJSONKey( const auto& aJSON, const std::string_view aKeyName )
 {
+	static_assert( is_json_v<decltype( aJSON )>, "The type of the JSON object is not supported." );
 	return aJSON.at( aKeyName ).template get<T>();
 }
 
 template<typename T>
 T ValueFromOptionalJSONKey( const auto& aJSON, const std::string_view aKeyName, const T& aDefault )
 {
+	static_assert( is_json_v<decltype( aJSON )>, "The type of the JSON object is not supported." );
 	const auto& found = aJSON.find( aKeyName );
 	return found != aJSON.cend() ? ( *found ).template get<T>() : aDefault;
 }
