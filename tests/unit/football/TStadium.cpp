eafd0e7d5b4ea7aff50a2d5658b3b9a8a17c1acf
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-22 12:09:33 +0100

    Test JSON constructor

--- tests/unit/football/TStadium.cpp
+++ tests/unit/football/TStadium.cpp
@@ -2,6 +2,8 @@
 
 #include "football/CStadium.h"
 
+#include "JsonUtils.h"
+
 #include <iostream>
 
 using namespace futsim::football;
@@ -16,6 +18,33 @@ void TStadium::TestExceptions() const
 	CheckException( []() { CStadium{ "Camp Nou", 98000, -2 }; }, "The ambient factor cannot be negative." );
 
 	//! Test JSON constructor
+	CheckException( []() { futsim::ValueFromJSON<CStadium>( json::parse( R"( {
+			"Stadium": {}
+		} )" ) ); }, "key 'Name' not found" );
+	CheckException( []() { futsim::ValueFromJSON<CStadium>( json::parse( R"( {
+			"Stadium": {
+				"Name": ""
+			}
+		} )" ) ); }, "The stadium name cannot be empty." );
+	CheckException( []() { futsim::ValueFromJSON<CStadium>( json::parse( R"( {
+			"Stadium": {
+				"Name": "Camp Nou"
+			}
+		} )" ) ); }, "key 'Capacity' not found" );
+	CheckException( []() { futsim::ValueFromJSON<CStadium>( json::parse( R"( {
+			"Stadium": {
+				"Name": "Camp Nou",
+				"Capacity": 98000
+			}
+		} )" ) ); }, "key 'Ambient factor' not found" );
+
+	CheckException( []() { futsim::ValueFromJSON<CStadium>( json::parse( R"( {
+			"Stadium": {
+				"Name": "Camp Nou",
+				"Capacity": 98000,
+				"Ambient factor": -2
+			}
+		} )" ) ); }, "The ambient factor cannot be negative." );
 }
 
 std::vector<std::string> TStadium::ObtainedResults() const noexcept
