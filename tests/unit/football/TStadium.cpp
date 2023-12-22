87e20d55bda9e9c6dfdca25141efceee61f29a9f
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-22 12:25:21 +0100

    Test outputs

--- tests/unit/football/TStadium.cpp
+++ tests/unit/football/TStadium.cpp
@@ -50,11 +50,61 @@ void TStadium::TestExceptions() const
 std::vector<std::string> TStadium::ObtainedResults() const noexcept
 {
 	std::vector<std::string> result;
+
+	for( const auto& stadium : {
+		CStadium{ "Camp Nou", 98000, 0.8 },
+		CStadium{ "The New Lawn", 5147, 0.5 },
+		futsim::ValueFromJSON<CStadium>( json::parse( R"( {
+			"Stadium": {
+				"Name": "Camp Nou",
+				"Capacity": 98000,
+				"Ambient factor": 0.8
+			}
+		} )" ) ),
+		futsim::ValueFromJSON<CStadium>( json::parse( R"( {
+			"Stadium": {
+				"Name": "The New Lawn",
+				"Capacity": 5147,
+				"Ambient factor": 0.5
+			}
+		} )" ) ) } )
+	{
+		result.push_back( std::string{ CStadium::JSON_NAME } + ": " + std::string{ stadium.GetName() } );
+		result.push_back( std::string{ CStadium::JSON_CAPACITY } + ": " + std::to_string( stadium.GetCapacity() ) );
+		result.push_back( std::string{ CStadium::JSON_AMBIENT_FACTOR } + ": " + std::to_string( stadium.GetAmbientFactor() ) );
+		futsim::IJsonableTypes::json outputJSON;
+		outputJSON[ CStadium::JSON_KEY ] = stadium.ToJSON();
+		result.push_back( outputJSON.dump( 1, '\t' ) );
+	}
+
 	return result;
 }
 
 std::vector<std::string> TStadium::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
+	std::vector<std::string> result{
+		"Name: Camp Nou",
+		"Capacity: 98000",
+		"Ambient factor: 0.800000",
+		"{\n"
+		"	\"Stadium\": {\n"
+		"		\"Name\": \"Camp Nou\",\n"
+		"		\"Capacity\": 98000,\n"
+		"		\"Ambient factor\": 0.8\n"
+		"	}\n"
+		"}",
+		"Name: The New Lawn",
+		"Capacity: 5147",
+		"Ambient factor: 0.500000",
+		"{\n"
+		"	\"Stadium\": {\n"
+		"		\"Name\": \"The New Lawn\",\n"
+		"		\"Capacity\": 5147,\n"
+		"		\"Ambient factor\": 0.5\n"
+		"	}\n"
+		"}"
+	};
+	result.reserve( 2 * result.size() );
+	result.insert( result.cend(), result.cbegin(), result.cend() );
 	return result;
 }
