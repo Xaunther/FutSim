73173b87a88d5b858efe01e80be8e2fc8f3e44f2
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-22 09:17:33 +0100

    Add stadium members

--- include/football/CStadium.h
+++ include/football/CStadium.h
@@ -2,11 +2,28 @@
 
 #include "IJsonable.h"
 
+#include "CStadiumTypes.h"
+
 namespace futsim
+{
+
+namespace football
 {
 
 class CStadium : public IJsonable
 {
+	using json = IJsonableTypes::json;
+	using capacity = CStadiumTypes::capacity;
+	using ambient_factor = CStadiumTypes::ambient_factor;
+
+	//! Stadium name.
+	std::string mName;
+	//! Stadium capacity.
+	capacity mCapacity;
+	//! Ambient factor that dictates the support the home team receives.
+	ambient_factor mAmbientFactor;
 };
+
+} // football namespace
 
 } // futsim namespace
