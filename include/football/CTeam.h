2fc521098edb189eb4af91d2d6fdcf379418c855
Alejandro Alfonso <alex.1993.24@gmail.com>
2023-12-23 00:22:11 +0100

    Add class members

--- include/football/CTeam.h
+++ include/football/CTeam.h
@@ -2,16 +2,32 @@
 
 #include "IJsonable.h"
 
-namespace futsim
-{
+#include "football/CPlayer.h"
+#include "football/CTeamTypes.h"
 
-namespace football
+#include <random>
+
+namespace futsim::football
 {
 
 class CTeam : public IJsonable
 {
+	using name_type = CTeamTypes::name_type;
+	using players = CTeamTypes::players;
+	using support_factor = CTeamTypes::support_factor;
+
+	//! Team name.
+	name_type mName;
+	//! 3-letter team abbreviation.
+	name_type mAbbreviation;
+	//! Manager.
+	name_type mManager;
+	//! Roster of players.
+	players mPlayers;
+	//! Support factor that dictates the support the team receives from the fans.
+	support_factor mSupportFactor;
+	//! Local fan attendance distribution
+	std::normal_distribution<> mAttendanceDistribution;
 };
 
-} // football namespace
-
-} // futsim namespace
+} // futsim::football namespace
