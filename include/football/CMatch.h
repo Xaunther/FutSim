3e4cbbb7394a2682da4e79878e1a184af72ae69d
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-11 12:26:49 +0100

    Add JSON constructor

--- include/football/CMatch.h
+++ include/football/CMatch.h
@@ -10,6 +10,7 @@ namespace futsim::football
 
 class CMatch : public IJsonable
 {
+	using json = IJsonableTypes::json;
 	using name_type = CTeamTypes::name_type;
 
 public:
@@ -26,6 +27,21 @@ public:
 		const CTeam& aAwayTeam,
 		const CStadium& aStadium,
 		const std::string_view aReferee );
+
+	/**
+	 * @brief JSON constructor.
+	 * @param aJSON JSON object.
+	*/
+	explicit CMatch( const json& aJSON );
+
+	//! JSON key for the class.
+	static inline constexpr std::string_view JSON_KEY = "Match";
+	//! JSON key for the \copybrief mHomeTeam
+	static inline constexpr std::string_view JSON_HOME_TEAM = "Home team";
+	//! JSON key for the \copybrief mAwayTeam
+	static inline constexpr std::string_view JSON_AWAY_TEAM = "Away team";
+	//! JSON key for the \copybrief mReferee
+	static inline constexpr std::string_view JSON_REFEREE = "Referee";
 
 private:
 	//! Home team.
--- src/football/CMatch.cpp
+++ src/football/CMatch.cpp
@@ -1,6 +1,7 @@
 #include "football/CMatch.h"
 
 #include "ExceptionUtils.h"
+#include "JsonUtils.h"
 #include "NameUtils.h"
 
 namespace futsim::football
@@ -19,5 +20,14 @@ CMatch::CMatch(
 {
 }
 FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the match." )
+
+CMatch::CMatch( const json& aJSON ) try :
+	mHomeTeam( ValueFromRequiredJSONKey<CTeam>( aJSON, JSON_HOME_TEAM ) ),
+	mAwayTeam( ValueFromRequiredJSONKey<CTeam>( aJSON, JSON_AWAY_TEAM ) ),
+	mStadium( ValueFromRequiredJSONKey<CStadium>( aJSON ) ),
+	mReferee( CheckName( ValueFromRequiredJSONKey<name_type>( aJSON, JSON_REFEREE ), "referee name" ) )
+{
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the match from JSON." )
 
 } // futsim::football namespace
