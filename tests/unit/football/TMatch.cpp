8f1d7eebbc29d5253d0c8744a23a43f048e0d4e8
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-11 12:40:48 +0100

    Test JSON constructor

--- tests/unit/football/TMatch.cpp
+++ tests/unit/football/TMatch.cpp
@@ -18,6 +18,94 @@ void TMatch::TestExceptions() const
 		CTeam{ "Luton Town FC", "lut", "Rob Edwards", {}, 1, 11000, 2000 },
 		CTeam{ "Luton Town FC", "lut", "Rob Edwards", {}, 1, 11000, 2000 },
 		CStadium{ "Camp Nou", 98000, 1 }, "" }; }, "The referee name cannot be empty." );
+
+	//! Test JSON constructor
+	CheckException( []() { futsim::ValueFromJSONKeyString<CMatch>( R"( {
+			"Match": {}
+		} )" ); }, "key 'Home team' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CMatch>( R"( {
+			"Match": {
+				"Home team": {
+					"Name": "Luton Town FC",
+					"Abbreviation": "lut",
+					"Manager": "Rob Edwards",
+					"Support factor": 1,
+					"Mean attendance": 11000,
+					"StdDev attendance": 1000
+				}
+			}
+		} )" ); }, "key 'Away team' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CMatch>( R"( {
+			"Match": {
+				"Home team": {
+					"Name": "Luton Town FC",
+					"Abbreviation": "lut",
+					"Manager": "Rob Edwards",
+					"Support factor": 1,
+					"Mean attendance": 11000,
+					"StdDev attendance": 1000
+				},
+				"Away team": {
+					"Name": "Luton Town FC",
+					"Abbreviation": "lut",
+					"Manager": "Rob Edwards",
+					"Support factor": 1,
+					"Mean attendance": 11000,
+					"StdDev attendance": 1000
+				}
+			}
+		} )" ); }, "key 'Stadium' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CMatch>( R"( {
+			"Match": {
+				"Home team": {
+					"Name": "Luton Town FC",
+					"Abbreviation": "lut",
+					"Manager": "Rob Edwards",
+					"Support factor": 1,
+					"Mean attendance": 11000,
+					"StdDev attendance": 1000
+				},
+				"Away team": {
+					"Name": "Luton Town FC",
+					"Abbreviation": "lut",
+					"Manager": "Rob Edwards",
+					"Support factor": 1,
+					"Mean attendance": 11000,
+					"StdDev attendance": 1000
+				},
+				"Stadium": {
+					"Name": "Camp Nou",
+					"Capacity": 98000,
+					"Ambient factor": 1
+				}
+			}
+		} )" ); }, "key 'Referee' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CMatch>( R"( {
+			"Match": {
+				"Home team": {
+					"Name": "Luton Town FC",
+					"Abbreviation": "lut",
+					"Manager": "Rob Edwards",
+					"Support factor": 1,
+					"Mean attendance": 11000,
+					"StdDev attendance": 1000
+				},
+				"Away team": {
+					"Name": "Luton Town FC",
+					"Abbreviation": "lut",
+					"Manager": "Rob Edwards",
+					"Support factor": 1,
+					"Mean attendance": 11000,
+					"StdDev attendance": 1000
+				},
+				"Stadium": {
+					"Name": "Camp Nou",
+					"Capacity": 98000,
+					"Ambient factor": 1
+				},
+				"Referee": ""
+			}
+		} )" ); }, "The referee name cannot be empty." );
 }
 
 std::vector<std::string> TMatch::ObtainedResults() const noexcept
