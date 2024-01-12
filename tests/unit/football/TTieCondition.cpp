4d24270177ae9687eeed92fb57917029ddf1de11
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-12 13:41:08 +0100

    Test JSON constructor

--- tests/unit/football/TTieCondition.cpp
+++ tests/unit/football/TTieCondition.cpp
@@ -15,6 +15,17 @@ void TTieCondition::TestExceptions() const
 {
 	//! Test member constructor.
 	CheckException( []() { CTieCondition{ 5, 1 }; }, "The home team goals cannot be smaller than the home team lead." );
+
+	//! Test JSON constructor.
+	CheckException( []() { futsim::ValueFromJSONKeyString<CTieCondition>( R"( {
+			"Tie condition": {}
+		} )" ); }, "key 'Home team lead' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CTieCondition>( R"( {
+			"Tie condition": {
+				"Home team lead" : 5,
+				"Home team goals": 1
+			}
+		} )" ); }, "The home team goals cannot be smaller than the home team lead." );
 }
 
 std::vector<std::string> TTieCondition::ObtainedResults() const noexcept
