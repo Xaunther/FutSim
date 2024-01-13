9b8590e4ae8b8bc7fc214090b8eff36c78c50e9a
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-13 09:39:10 +0100

    Add play time types

---
+++ include/CPlayTimeTypes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * @brief Types defined for \ref CPlayTime.
+*/
+namespace futsim::CPlayTimeTypes
+{
+
+//! Type for the number of periods.
+using period_count = unsigned int;
+
+//! Type for the time of a period, in minutes.
+using period_time = unsigned int;
+
+} // futsim::CPlayTimeTypes namespace
