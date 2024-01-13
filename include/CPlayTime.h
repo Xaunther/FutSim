67780323287982b18befe3b087daac53fa28d672
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-13 09:54:01 +0100

    Add member constructor

--- include/CPlayTime.h
+++ include/CPlayTime.h
@@ -15,6 +15,19 @@ class CPlayTime : public IJsonable
 	using period_count = CPlayTimeTypes::period_count;
 	using period_time = CPlayTimeTypes::period_time;
 
+public:
+	/**
+	 * @brief Member constructor.
+	 * @param aPeriodCount \ref mPeriodCount
+	 * @param aPeriodTime \ref mPeriodTime
+	 * @pre At least 1 period must compose the play time.
+	 * @pre Each period must be at least 1 minute long.
+	*/
+	explicit CPlayTime(
+		const period_count& aPeriodCount,
+		const period_time& aPeriodTime );
+
+private:
 	//! Number of periods.
 	period_count mPeriodCount;
 	//! Length of each period.
---
+++ src/CPlayTime.cpp
@@ -0,0 +1,43 @@
+#include "CPlayTime.h"
+
+#include "ExceptionUtils.h"
+
+namespace futsim
+{
+
+namespace
+{
+
+/**
+ * @brief Checks that a number is not zero.
+ * @param aNumber Number.
+ * @param aNameString String with the name label to add to the error message.
+*/
+template<typename T> const T& CheckNonZero( const T& aNumber, const std::string_view aNameString );
+
+} // anonymous namespace
+
+CPlayTime::CPlayTime(
+	const period_count& aPeriodCount,
+	const period_time& aPeriodTime
+) try :
+	mPeriodCount( CheckNonZero( aPeriodCount, "number of periods" ) ),
+	mPeriodTime( CheckNonZero( aPeriodTime, "length of the period" ) )
+{
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the play time." )
+
+namespace
+{
+
+template<typename T> const T& CheckNonZero( const T& aNumber, const std::string_view aNameString ) try
+{
+	if( aNumber == 0 )
+		throw std::invalid_argument{ "The " + std::string( aNameString.data() ) + " cannot be zero." };
+	return aNumber;
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error checking the " << aNameString << "." )
+
+} // anonymous namespace
+
+} // futsim namespace
