2ab261e37574ec0611842154bbca158d3c38d1f8
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-15 08:46:42 +0100

    Test JSON constructor

--- tests/unit/TPlayTime.cpp
+++ tests/unit/TPlayTime.cpp
@@ -2,6 +2,8 @@
 
 #include "CPlayTime.h"
 
+#include "JsonUtils.h"
+
 #include <iostream>
 
 using namespace futsim;
@@ -16,6 +18,27 @@ void TPlayTime::TestExceptions() const
 		"The number of periods cannot be zero." );
 	CheckException( []() { CPlayTime{ 2, 0 }; },
 		"The length of the period cannot be zero." );
+
+	//! Test JSON constructor
+	CheckException( []() { ValueFromJSONKeyString<CPlayTime>( R"( {
+			"Play time": {}
+		} )" ); }, "key 'Period count' not found" );
+	CheckException( []() { ValueFromJSONKeyString<CPlayTime>( R"( {
+			"Play time": {
+				"Period count": 0
+			}
+		} )" ); }, "The number of periods cannot be zero." );
+	CheckException( []() { ValueFromJSONKeyString<CPlayTime>( R"( {
+			"Play time": {
+				"Period count": 2
+			}
+		} )" ); }, "key 'Period time' not found" );
+	CheckException( []() { ValueFromJSONKeyString<CPlayTime>( R"( {
+			"Play time": {
+				"Period count": 2,
+				"Period time": 0
+			}
+		} )" ); }, "The length of the period cannot be zero." );
 }
 
 std::vector<std::string> TPlayTime::ObtainedResults() const noexcept
