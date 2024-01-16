0fba2fec498279303f1fe3042756bdad0029649d
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-16 11:45:27 +0100

    Add member constructor

--- include/football/CPlayTime.h
+++ include/football/CPlayTime.h
@@ -13,7 +13,22 @@ namespace futsim::football
 class CPlayTime : public futsim::CPlayTime
 {
 	using subs_count = CPlayTimeTypes::subs_count;
+	using period_count = futsim::CPlayTimeTypes::period_count;
+	using period_time = futsim::CPlayTimeTypes::period_time;
 
+public:
+	/**
+	 * @copydoc futsim::CPlayTime::CPlayTime
+	 * @param aAvailableSubs \ref mAvailableSubs
+	 * @pre At least 1 period must compose the play time.
+	 * @pre Each period must be at least 1 minute long.
+	*/
+	explicit CPlayTime(
+		const period_count& aPeriodCount = 2,
+		const period_time& aPeriodTime = 45,
+		const subs_count& aAvailableSubs = 5 );
+
+private:
 	//! Number of available subs.
 	subs_count mAvailableSubs;
 };
--- src/football/CPlayTime.cpp
+++ src/football/CPlayTime.cpp
@@ -1,1 +1,19 @@
 #include "football/CPlayTime.h"
+
+#include "ExceptionUtils.h"
+
+namespace futsim::football
+{
+
+CPlayTime::CPlayTime(
+	const period_count& aPeriodCount,
+	const period_time& aPeriodTime,
+	const subs_count& aAvailableSubs
+) try :
+	futsim::CPlayTime( aPeriodCount, aPeriodTime ),
+	mAvailableSubs( aAvailableSubs )
+{
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the football play time from JSON." )
+
+} // futsim::football namespace
