d0ced01850b5004f36020057af7e9899abfe3d70
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-16 11:37:33 +0100

    Add football play time types

---
+++ include/football/CPlayTimeTypes.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+ * @brief Types defined for \ref CPlayTimeTypes.
+*/
+namespace futsim::football::CPlayTimeTypes
+{
+
+//! Type for the number of substitutions.
+using subs_count = unsigned int;
+
+} // futsim::football::CPlayTimeTypes namespace
