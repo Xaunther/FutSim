15960b23610699806b375d2b42ec5a0f8070174d
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-22 11:20:04 +0100

    Add member constructor

--- include/football/CExtraTime.h
+++ include/football/CExtraTime.h
@@ -12,6 +12,22 @@ namespace futsim::football
 */
 class CExtraTime : public CPlayTime
 {
+	using subs_count = CPlayTimeTypes::subs_count;
+	using period_count = futsim::CPlayTimeTypes::period_count;
+	using period_time = futsim::CPlayTimeTypes::period_time;
+
+public:
+	/**
+	 * @copydoc futsim::football::CPlayTime::CPlayTime
+	 * @param aGoalRule \ref mGoalRule
+	*/
+	explicit CExtraTime(
+		const period_count& aPeriodCount = 2,
+		const period_time& aPeriodTime = 15,
+		const subs_count& aAvailableSubs = 1,
+		const E_GOAL_RULE& aGoalRule = E_GOAL_RULE::NO );
+
+private:
 	//! Goal rule.
 	E_GOAL_RULE mGoalRule;
 };
---
+++ src/football/CExtraTime.cpp
@@ -0,0 +1,21 @@
+#include "football/CExtraTime.h"
+
+#include "ExceptionUtils.h"
+#include "JsonUtils.h"
+
+namespace futsim::football
+{
+
+CExtraTime::CExtraTime(
+	const period_count& aPeriodCount,
+	const period_time& aPeriodTime,
+	const subs_count& aAvailableSubs,
+	const E_GOAL_RULE& aGoalRule
+) try :
+	CPlayTime( aPeriodCount, aPeriodTime, aAvailableSubs ),
+	mGoalRule( aGoalRule )
+{
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the football extra time." )
+
+} // futsim::football namespace
