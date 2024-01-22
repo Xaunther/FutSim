814cf2922c450de4b4344d4c7a8039c8707ade1a
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-22 12:47:19 +0100

    Make CPlayTime types protected

--- include/football/CPlayTime.h
+++ include/football/CPlayTime.h
@@ -13,8 +13,6 @@ namespace futsim::football
 class CPlayTime : public futsim::CPlayTime
 {
 	using subs_count = CPlayTimeTypes::subs_count;
-	using period_count = futsim::CPlayTimeTypes::period_count;
-	using period_time = futsim::CPlayTimeTypes::period_time;
 
 public:
 	/**
