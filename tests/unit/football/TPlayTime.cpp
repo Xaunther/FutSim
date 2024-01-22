5652f550c05b4b9ad71d6030caabe86173c0bb77
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-22 08:48:27 +0100

    Test JSON constructor

--- tests/unit/football/TPlayTime.cpp
+++ tests/unit/football/TPlayTime.cpp
@@ -2,6 +2,8 @@
 
 #include "football/CPlayTime.h"
 
+#include "JsonUtils.h"
+
 #include <iostream>
 
 using namespace futsim::football;
@@ -11,6 +13,12 @@ INITIALIZE_TEST( TPlayTime )
 
 void TPlayTime::TestExceptions() const
 {
+	CheckException( []() { futsim::ValueFromJSONKeyString<CPlayTime>( R"( {
+			"Play time": {
+				"Period count": 2,
+				"Period time": 45
+			}
+		} )" ); }, "key 'Available subs' not found" );
 }
 
 std::vector<std::string> TPlayTime::ObtainedResults() const noexcept
