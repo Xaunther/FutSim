0fc7810645f2fbe8e55a4b9f282a82fd11dfc9c5
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-26 13:42:56 +0100

    Define CMatchConfiguration class

--- FileList.cmake
+++ FileList.cmake
@@ -16,6 +16,7 @@ SET(FUTSIM_CORE_EXTERNAL_HEADERS
 SET(FUTSIM_FOOTBALL_EXTERNAL_HEADERS
 	include/football/CExtraTime.h
 	include/football/CMatch.h
+	include/football/CMatchConfiguration.h
 	include/football/CPenaltyShootoutConfiguration.h
 	include/football/CPenaltyShootoutConfigurationTypes.h
 	include/football/CPlayer.h
---
+++ include/football/CMatchConfiguration.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "IJsonable.h"
+
+namespace futsim::football
+{
+
+/**
+ * @brief Class that configures a football match.
+*/
+class CMatchConfiguration : public IJsonable
+{
+};
+
+} // futsim namespace
