2a40ca3f3615337fa71f29cb1b3442dd0de09129
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-01-26 13:57:31 +0100

    Add members to match configuration

--- include/football/CMatchConfiguration.h
+++ include/football/CMatchConfiguration.h
@@ -2,6 +2,12 @@
 
 #include "IJsonable.h"
 
+#include "football/CExtraTime.h"
+#include "football/CMatchConfigurationTypes.h"
+#include "football/CPenaltyShootoutConfiguration.h"
+#include "football/CPlayTime.h"
+#include "football/CTieCondition.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +16,25 @@ namespace futsim::football
 */
 class CMatchConfiguration : public IJsonable
 {
+protected:
+	using benched_count = CMatchConfigurationTypes::benched_count;
+	using optional_tie_condition = CMatchConfigurationTypes::optional_tie_condition;
+	using optional_extra_time = CMatchConfigurationTypes::optional_extra_time;
+	using optional_penalty_shootout_configuration = CMatchConfigurationTypes::optional_penalty_shootout_configuration;
+
+private:
+	//! Play time configuration.
+	CPlayTime mPlayTime;
+	//! Number of players on the bench. Empty for unlimited number.
+	benched_count mBenchedPlayersCount;
+	//! Whether the ambient factor applies to the home team. Useful to play on neutral grounds.
+	bool mApplyAmbientFactor;
+	//! Tie condition.
+	optional_tie_condition mTieCondition;
+	//! Extra time configuration.
+	optional_extra_time mExtraTime;
+	//! Penalty shootout configuration.
+	optional_penalty_shootout_configuration mPenaltyShootoutConfiguration;
 };
 
 } // futsim namespace
