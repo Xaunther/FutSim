80c9d130c9ce885c281d1849dd0667c8e5860022
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 14:31:56 +0100

    Add class members

--- include/football/CChancesDrawConfiguration.h
+++ include/football/CChancesDrawConfiguration.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "IJsonable.h"
+
+#include "football/CChancesDrawConfigurationTypes.h"
 namespace futsim::football
 {
 
@@ -9,6 +11,53 @@ namespace futsim::football
 */
 class CChancesDrawConfiguration : public IJsonable
 {
+protected:
+	using stat = CChancesDrawConfigurationTypes::stat;
+
+public:
+	//! Retrieves the \copybrief mAverageChances
+	const stat& GetAverageChances() const noexcept;
+
+	//! Retrieves the \copybrief mAverageCornerKicks
+	const stat& GetAverageCornerKicks() const noexcept;
+
+	//! Retrieves the \copybrief mAverage1vs1GKs
+	const stat& GetAverage1vs1GKs() const noexcept;
+
+	//! Retrieves the \copybrief mAverage1vs1DFs
+	const stat& GetAverage1vs1DFs() const noexcept;
+
+	//! Retrieves the \copybrief mAverageNearShots
+	const stat& GetAverageNearShots() const noexcept;
+
+	//! Retrieves the \copybrief mAverageSetPieces
+	const stat& GetAverageSetPieces() const noexcept;
+
+	//! Retrieves the \copybrief mAveragePenalties
+	const stat& GetAveragePenalties() const noexcept;
+
+	//! Retrieves the \copybrief mAverageDirectFreeKicks
+	const stat& GetAverageDirectFreeKicks() const noexcept;
+
+private:
+	//! Average number of chances per 90 minutes.
+	stat mAverageChances;
+
+	//! Average number of corner kicks per 90 minutes.
+	stat mAverageCornerKicks;
+	//! Average number of 1 on 1 vs GK chances per 90 minutes.
+	stat mAverage1vs1GKs;
+	//! Average number of 1 on 1 vs DF chances per 90 minutes.
+	stat mAverage1vs1DFs;
+	//! Average number of near shots per 90 minutes.
+	stat mAverageNearShots;
+
+	//! Average number of set pieces per 90 minutes.
+	stat mAverageSetPieces;
+	//! Average number of penalties per 90 minutes.
+	stat mAveragePenalties;
+	//! Average number of direct free kicks per 90 minutes.
+	stat mAverageDirectFreeKicks;
 };
 
 } // futsim namespace
--- src/football/CChancesDrawConfiguration.cpp
+++ src/football/CChancesDrawConfiguration.cpp
@@ -1,1 +1,46 @@
 #include "football/CChancesDrawConfiguration.h"
+
+namespace futsim::football
+{
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAverageChances() const noexcept
+{
+	return mAverageChances;
+}
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAverageCornerKicks() const noexcept
+{
+	return mAverageCornerKicks;
+}
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAverage1vs1GKs() const noexcept
+{
+	return mAverage1vs1GKs;
+}
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAverage1vs1DFs() const noexcept
+{
+	return mAverage1vs1DFs;
+}
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAverageNearShots() const noexcept
+{
+	return mAverageNearShots;
+}
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAverageSetPieces() const noexcept
+{
+	return mAverageSetPieces;
+}
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAveragePenalties() const noexcept
+{
+	return mAveragePenalties;
+}
+
+const CChancesDrawConfiguration::stat& CChancesDrawConfiguration::GetAverageDirectFreeKicks() const noexcept
+{
+	return mAverageDirectFreeKicks;
+}
+
+} // futsim::football namespace
