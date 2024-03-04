b12fc2b1d42c15e7652c6d572f5d8ed97987a343
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 11:41:09 +0100

    Add member constructor

--- include/football/CFoulDrawConfiguration.h
+++ include/football/CFoulDrawConfiguration.h
@@ -16,6 +16,18 @@ protected:
 	using stat = CFoulDrawConfigurationTypes::stat;
 
 public:
+	/**
+	 * @brief Member constructor.
+	 * @param aAverageFouls \ref mAverageFouls
+	 * @param aAverageYellowCards \ref mAverageYellowCards
+	 * @param aAverageRedCards \ref mAverageRedCards
+	*/
+	explicit CFoulDrawConfiguration(
+		const stat& aAverageFouls = DEFAULT_AVERAGE_FOULS,
+		const stat& aAverageYellowCards = DEFAULT_AVERAGE_YELLOW_CARDS,
+		const stat& aAverageRedCards = DEFAULT_AVERAGE_RED_CARDS
+	);
+
 	//! Retrieves the \copybrief mAverageFouls
 	const stat& GetAverageFouls() const noexcept;
 
@@ -24,6 +36,13 @@ public:
 
 	//! Retrieves the \copybrief mAverageRedCards
 	const stat& GetAverageRedCards() const noexcept;
+
+	//! Default \copybrief mAverageFouls
+	static inline constexpr stat DEFAULT_AVERAGE_FOULS = stat{ 8195 } / 380;
+	//! Default \copybrief mAverageYellowCards
+	static inline constexpr stat DEFAULT_AVERAGE_YELLOW_CARDS = stat{ 1375 } / 380;
+	//! Default \copybrief mAverageRedCards
+	static inline constexpr stat DEFAULT_AVERAGE_RED_CARDS = stat{ 31 } / 380;
 
 private:
 	//! Average number of fouls per 90 minutes.
--- src/football/CFoulDrawConfiguration.cpp
+++ src/football/CFoulDrawConfiguration.cpp
@@ -1,7 +1,23 @@
 #include "football/CFoulDrawConfiguration.h"
 
+#include "ExceptionUtils.h"
+#include "NumberUtils.h"
+
 namespace futsim::football
 {
+
+CFoulDrawConfiguration::CFoulDrawConfiguration(
+	const stat& aAverageFouls,
+	const stat& aAverageYellowCards,
+	const stat& aAverageRedCards
+) try :
+	mAverageFouls( CheckNonNegativeness( aAverageFouls, "average number of fouls" ) ),
+	mAverageYellowCards( CheckNonNegativeness( aAverageYellowCards, "average number of yellow cards" ) ),
+	mAverageRedCards( CheckNonNegativeness( aAverageRedCards, "average number of red cards" ) )
+{
+	CheckNonNegativeness( mAverageFouls - mAverageYellowCards - mAverageRedCards, "average number of fouls minus the average number of yellow and red cards" );
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the foul draw configuration." )
 
 const CFoulDrawConfiguration::stat& CFoulDrawConfiguration::GetAverageFouls() const noexcept
 {
