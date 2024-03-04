a3f7971644a6676625c25c3bad74692f1aadae97
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 10:54:03 +0100

    Test output

--- tests/unit/football/TPossessionDrawConfiguration.cpp
+++ tests/unit/football/TPossessionDrawConfiguration.cpp
@@ -36,12 +36,32 @@ void TPossessionDrawConfiguration::TestExceptions() const
 std::vector<std::string> TPossessionDrawConfiguration::ObtainedResults() const noexcept
 {
 	std::vector<std::string> result;
+	for( const auto& possessionDrawConfiguration : {
+		CPossessionDrawConfiguration{ 295952.0 / 460939.0 },
+		futsim::ValueFromJSONKeyString<CPossessionDrawConfiguration>( R"( {
+			"Possession draw configuration": {
+				"Keep possession probability": 0.6420632665059802
+			}
+		} )" ) } )
+	{
+		result.push_back( std::string{ CPossessionDrawConfiguration::JSON_KEEP_POSSESSION_PROBABILITY } + ": " + std::to_string( possessionDrawConfiguration.GetKeepPossessionProbability() ) );
+		futsim::IJsonableTypes::json outputJSON;
+		AddToJSONKey( outputJSON, possessionDrawConfiguration );
+		result.push_back( outputJSON.dump( 1, '\t' ) );
+	}
 	return result;
 }
 
 std::vector<std::string> TPossessionDrawConfiguration::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
+	std::vector<std::string> result{
+		"Keep possession probability: 0.642063",
+		"{\n"
+		"	\"Possession draw configuration\": {\n"
+		"		\"Keep possession probability\": 0.6420632665059802\n"
+		"	}\n"
+		"}"
+	};
 	result.reserve( 2 * result.size() );
 	result.insert( result.cend(), result.cbegin(), result.cend() );
 	return result;
