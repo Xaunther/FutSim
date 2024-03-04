d5412348e0f2a0cb262db0f4ba7c539a321237f9
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 14:17:19 +0100

    Add chances draw configuration

---
+++ include/football/CChancesDrawConfiguration.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "IJsonable.h"
+namespace futsim::football
+{
+
+/**
+ * @brief Class that configures the draws of the different chances.
+*/
+class CChancesDrawConfiguration : public IJsonable
+{
+};
+
+} // futsim namespace
