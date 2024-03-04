b583c62d0666e08d08f0f3a67d55c92bdf324b48
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 11:14:08 +0100

    Add class members

--- include/football/CFoulDrawConfiguration.h
+++ include/football/CFoulDrawConfiguration.h
@@ -2,6 +2,8 @@
 
 #include "IJsonable.h"
 
+#include "football/CFoulDrawConfigurationTypes.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +12,26 @@ namespace futsim::football
 */
 class CFoulDrawConfiguration : public IJsonable
 {
+protected:
+	using stat = CFoulDrawConfigurationTypes::stat;
+
+public:
+	//! Retrieves the \copybrief mAverageFouls
+	const stat& GetAverageFouls() const noexcept;
+
+	//! Retrieves the \copybrief mAverageYellowCards
+	const stat& GetAverageYellowCards() const noexcept;
+
+	//! Retrieves the \copybrief mAverageRedCards
+	const stat& GetAverageRedCards() const noexcept;
+
+private:
+	//! Average number of fouls per 90 minutes.
+	stat mAverageFouls;
+	//! Average number of yellow cards per 90 minutes.
+	stat mAverageYellowCards;
+	//! Average number of red cards per 90 minutes.
+	stat mAverageRedCards;
 };
 
 } // futsim namespace
--- src/football/CFoulDrawConfiguration.cpp
+++ src/football/CFoulDrawConfiguration.cpp
@@ -1,1 +1,21 @@
 #include "football/CFoulDrawConfiguration.h"
+
+namespace futsim::football
+{
+
+const CFoulDrawConfiguration::stat& CFoulDrawConfiguration::GetAverageFouls() const noexcept
+{
+	return mAverageFouls;
+}
+
+const CFoulDrawConfiguration::stat& CFoulDrawConfiguration::GetAverageYellowCards() const noexcept
+{
+	return mAverageYellowCards;
+}
+
+const CFoulDrawConfiguration::stat& CFoulDrawConfiguration::GetAverageRedCards() const noexcept
+{
+	return mAverageRedCards;
+}
+
+} // futsim::football namespace
