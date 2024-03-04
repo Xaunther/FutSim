4bae27994317c86b9a9cfed07d6e5cbecb7cdde5
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 10:23:40 +0100

    Add possession draw configuration test

---
+++ tests/unit/football/TPossessionDrawConfiguration.cpp
@@ -0,0 +1,30 @@
+#include "ITest.h"
+
+#include "football/CPossessionDrawConfiguration.h"
+
+#include "JsonUtils.h"
+
+#include <iostream>
+
+using namespace futsim::football;
+using namespace nlohmann;
+
+INITIALIZE_TEST( TPossessionDrawConfiguration )
+
+void TPossessionDrawConfiguration::TestExceptions() const
+{
+}
+
+std::vector<std::string> TPossessionDrawConfiguration::ObtainedResults() const noexcept
+{
+	std::vector<std::string> result;
+	return result;
+}
+
+std::vector<std::string> TPossessionDrawConfiguration::ExpectedResults() const noexcept
+{
+	std::vector<std::string> result;
+	result.reserve( 2 * result.size() );
+	result.insert( result.cend(), result.cbegin(), result.cend() );
+	return result;
+}
