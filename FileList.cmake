d3d7e5f764ee480b133fd5d89a895d706b10ae5e
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 11:04:39 +0100

    Add CFoulDrawConfiguration class

--- FileList.cmake
+++ FileList.cmake
@@ -16,6 +16,7 @@ SET(FUTSIM_CORE_EXTERNAL_HEADERS
 # Football external headers
 SET(FUTSIM_FOOTBALL_EXTERNAL_HEADERS
 	include/football/CExtraTime.h
+	include/football/CFoulDrawConfiguration.h
 	include/football/CMatch.h
 	include/football/CMatchConfiguration.h
 	include/football/CMatchConfigurationTypes.h
@@ -49,6 +50,7 @@ SET(FUTSIM_CORE_SOURCE
 # Football source files
 SET(FUTSIM_FOOTBALL_SOURCE
 	src/football/CExtraTime.cpp
+	src/football/CFoulDrawConfiguration.cpp
 	src/football/CMatch.cpp
 	src/football/CMatchConfiguration.cpp
 	src/football/CPenaltyShootoutConfiguration.cpp
---
+++ include/football/CFoulDrawConfiguration.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "IJsonable.h"
+
+namespace futsim::football
+{
+
+/**
+ * @brief Class that configures the foul severity draw.
+*/
+class CFoulDrawConfiguration : public IJsonable
+{
+};
+
+} // futsim namespace
