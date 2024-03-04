7f763a0a7b0a765532cdbc63afbb622ad9de3736
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-04 12:41:59 +0100

    Test outputs

--- tests/unit/football/TFoulDrawConfiguration.cpp
+++ tests/unit/football/TFoulDrawConfiguration.cpp
@@ -48,12 +48,62 @@ void TFoulDrawConfiguration::TestExceptions() const
 std::vector<std::string> TFoulDrawConfiguration::ObtainedResults() const noexcept
 {
 	std::vector<std::string> result;
+	for( const auto& foulDrawConfiguration : {
+		CFoulDrawConfiguration{},
+		CFoulDrawConfiguration{ 10, 4, 1 },
+		futsim::ValueFromJSONKeyString<CFoulDrawConfiguration>( R"( {
+			"Foul draw configuration": {}
+		} )" ),
+		futsim::ValueFromJSONKeyString<CFoulDrawConfiguration>( R"( {
+			"Foul draw configuration": {
+				"Average fouls": 10,
+				"Average yellow cards": 4,
+				"Average red cards": 1
+			}
+		} )" ) } )
+	{
+		result.push_back( std::string{ CFoulDrawConfiguration::JSON_AVERAGE_FOULS } + ": " + std::to_string( foulDrawConfiguration.GetAverageFouls() ) );
+		result.push_back( std::string{ CFoulDrawConfiguration::JSON_AVERAGE_YELLOW_CARDS } + ": " + std::to_string( foulDrawConfiguration.GetAverageYellowCards() ) );
+		result.push_back( std::string{ CFoulDrawConfiguration::JSON_AVERAGE_RED_CARDS } + ": " + std::to_string( foulDrawConfiguration.GetAverageRedCards() ) );
+		for( const auto& probability : foulDrawConfiguration.CreateFoulDistribution().probabilities() )
+			result.push_back( std::to_string( probability ) );
+		futsim::IJsonableTypes::json outputJSON;
+		AddToJSONKey( outputJSON, foulDrawConfiguration );
+		result.push_back( outputJSON.dump( 1, '\t' ) );
+	}
 	return result;
 }
 
 std::vector<std::string> TFoulDrawConfiguration::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
+	std::vector<std::string> result{
+		"Average fouls: 21.565789",
+		"Average yellow cards: 3.618421",
+		"Average red cards: 0.081579",
+		"0.167785",
+		"0.003783",
+		"0.828432",
+		"{\n"
+		"	\"Foul draw configuration\": {\n"
+		"		\"Average fouls\": 21.56578947368421,\n"
+		"		\"Average yellow cards\": 3.6184210526315788,\n"
+		"		\"Average red cards\": 0.08157894736842106\n"
+		"	}\n"
+		"}",
+		"Average fouls: 10.000000",
+		"Average yellow cards: 4.000000",
+		"Average red cards: 1.000000",
+		"0.400000",
+		"0.100000",
+		"0.500000",
+		"{\n"
+		"	\"Foul draw configuration\": {\n"
+		"		\"Average fouls\": 10.0,\n"
+		"		\"Average yellow cards\": 4.0,\n"
+		"		\"Average red cards\": 1.0\n"
+		"	}\n"
+		"}"
+	};
 	result.reserve( 2 * result.size() );
 	result.insert( result.cend(), result.cbegin(), result.cend() );
 	return result;
