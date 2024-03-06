b5eeeb1e8b8b37d4afa3b12a219d8a71df4ab564
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-06 12:06:15 +0100

    Add class members

--- include/football/CGoalDrawConfiguration.h
+++ include/football/CGoalDrawConfiguration.h
@@ -2,6 +2,8 @@
 
 #include "IJsonable.h"
 
+#include "football/CGoalDrawConfigurationTypes.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +12,60 @@ namespace futsim::football
 */
 class CGoalDrawConfiguration : public IJsonable
 {
+protected:
+	using probability = CGoalDrawConfigurationTypes::probability;
+	using stat = CGoalDrawConfigurationTypes::stat;
+
+public:
+	//! Retrieves the \copybrief mAverageGoals
+	const stat& GetAverageGoals() const noexcept;
+
+	//! Retrieves the \copybrief mAveragePenaltyGoals
+	const stat& GetAveragePenaltyGoals() const noexcept;
+
+	//! Retrieves the \copybrief mAverageDirectFreeKickGoals
+	const stat& GetAverageDirectFreeKickGoals() const noexcept;
+
+	//! Retrieves the \copybrief mAverageIndirectFreeKickGoals
+	const stat& GetAverageIndirectFreeKickGoals() const noexcept;
+
+	//! Retrieves the \copybrief mAverageCornerGoals
+	const stat& GetAverageCornerGoals() const noexcept;
+
+	//! Retrieves the \copybrief mAverageFarShotGoals
+	const stat& GetAverageFarShotGoals() const noexcept;
+
+	//! Retrieves the \copybrief m1vs1GKGoalProbability
+	const probability& Get1vs1GKGoalProbability() const noexcept;
+
+	//! Retrieves the \copybrief m1vs1DFGoalProbability
+	const probability& Get1vs1DFGoalProbability() const noexcept;
+
+	//! Retrieves the \copybrief mExtraCornerProbability
+	const probability& GetExtraCornerProbability() const noexcept;
+
+private:
+	//! Average number of goals per 90 minutes.
+	stat mAverageGoals;
+
+	//! Average number of penalty goals per 90 minutes.
+	stat mAveragePenaltyGoals;
+	//! Average number of direct free kick goals per 90 minutes.
+	stat mAverageDirectFreeKickGoals;
+	//! Average number of indirect free kick goals per 90 minutes.
+	stat mAverageIndirectFreeKickGoals;
+	//! Average number of corner kick goals per 90 minutes.
+	stat mAverageCornerGoals;
+	//! Average number of far shot goals per 90 minutes.
+	stat mAverageFarShotGoals;
+
+	//! Probability to score a goal in a 1 on 1 vs GK chance.
+	probability m1vs1GKGoalProbability;
+	//! Probability to score a goal in a 1 on 1 vs DF chance.
+	probability m1vs1DFGoalProbability;
+
+	//! Probability to get an extra corner after any chance.
+	probability mExtraCornerProbability;
 };
 
 } // futsim namespace
--- src/football/CGoalDrawConfiguration.cpp
+++ src/football/CGoalDrawConfiguration.cpp
@@ -1,1 +1,51 @@
 #include "football/CGoalDrawConfiguration.h"
+
+namespace futsim::football
+{
+
+const CGoalDrawConfiguration::stat& CGoalDrawConfiguration::GetAverageGoals() const noexcept
+{
+	return mAverageGoals;
+}
+
+const CGoalDrawConfiguration::stat& CGoalDrawConfiguration::GetAveragePenaltyGoals() const noexcept
+{
+	return mAveragePenaltyGoals;
+}
+
+const CGoalDrawConfiguration::stat& CGoalDrawConfiguration::GetAverageDirectFreeKickGoals() const noexcept
+{
+	return mAverageDirectFreeKickGoals;
+}
+
+const CGoalDrawConfiguration::stat& CGoalDrawConfiguration::GetAverageIndirectFreeKickGoals() const noexcept
+{
+	return mAverageIndirectFreeKickGoals;
+}
+
+const CGoalDrawConfiguration::stat& CGoalDrawConfiguration::GetAverageCornerGoals() const noexcept
+{
+	return mAverageCornerGoals;
+}
+
+const CGoalDrawConfiguration::stat& CGoalDrawConfiguration::GetAverageFarShotGoals() const noexcept
+{
+	return mAverageFarShotGoals;
+}
+
+const CGoalDrawConfiguration::probability& CGoalDrawConfiguration::Get1vs1GKGoalProbability() const noexcept
+{
+	return m1vs1GKGoalProbability;
+}
+
+const CGoalDrawConfiguration::probability& CGoalDrawConfiguration::Get1vs1DFGoalProbability() const noexcept
+{
+	return m1vs1DFGoalProbability;
+}
+
+const CGoalDrawConfiguration::probability& CGoalDrawConfiguration::GetExtraCornerProbability() const noexcept
+{
+	return mExtraCornerProbability;
+}
+
+} //futsim::football namespace
