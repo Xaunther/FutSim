a25c9a5ae0cbb2bfbc0eac8aae26d398b48ea2c1
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-06 17:10:01 +0100

    Add function to compute the modified probability

--- include/ProbabilityUtils.h
+++ include/ProbabilityUtils.h
@@ -20,4 +20,19 @@ const auto& CheckProbability( const auto& aValue, const std::string_view aNameSt
 }
 FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error checking the " + std::string( aNameString.data() ) + "." )
 
+/**
+ * @brief Computes the modified probability due to a skill difference.
+ * @param aMaxProbability Maximum possible probability.
+ * @param aDefaultProbability Probability when skills are equal.
+ * @param aDefenderSkill Defender skill.
+ * @param aAttackerSkill Attacker skill.
+ * @pre The maximum probability must be greater than the default probability.
+ * @pre the attacker and defender skills must be greater than 0.
+*/
+template <std::floating_point T>
+auto ModifiedProbability( const T& aMaxProbability, const T& aDefaultProbability, const T& aDefenderSkill, const T& aAttackerSkill )
+{
+	return aMaxProbability / ( 1 + ( aMaxProbability - aDefaultProbability ) * aDefenderSkill / ( aDefaultProbability * aAttackerSkill ) );
+}
+
 } // futsim namespace
