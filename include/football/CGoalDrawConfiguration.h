a7e2331bb66a917b2c48318ab4b4a46bca9f4d59
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-06 12:35:05 +0100

    Add member constructor

--- include/football/CGoalDrawConfiguration.h
+++ include/football/CGoalDrawConfiguration.h
@@ -17,6 +17,30 @@ protected:
 	using stat = CGoalDrawConfigurationTypes::stat;
 
 public:
+	/**
+	 * @brief Member constructor.
+	 * @param aAverageGoals \ref mAverageGoals
+	 * @param aAveragePenaltyGoals \ref mAveragePenaltyGoals
+	 * @param aAverageDirectFreeKickGoals \ref mAverageDirectFreeKickGoals
+	 * @param aAverageIndirectFreeKickGoals \ref mAverageIndirectFreeKickGoals
+	 * @param aAverageCornerGoals \ref mAverageCornerGoals
+	 * @param aAverageFarShotGoals \ref mAverageFarShotGoals
+	 * @param a1vs1GKGoalProbability \ref m1vs1GKGoalProbability
+	 * @param a1vs1DFGoalProbability \ref m1vs1DFGoalProbability
+	 * @param aExtraCornerProbability \ref mExtraCornerProbability
+	*/
+	explicit CGoalDrawConfiguration(
+		const stat& aAverageGoals = DEFAULT_AVERAGE_GOALS,
+		const stat& aAveragePenaltyGoals = DEFAULT_AVERAGE_PENALTY_GOALS,
+		const stat& aAverageDirectFreeKickGoals = DEFAULT_AVERAGE_DIRECT_FREE_KICK_GOALS,
+		const stat& aAverageIndirectFreeKickGoals = DEFAULT_AVERAGE_INDIRECT_FREE_KICK_GOALS,
+		const stat& aAverageCornerGoals = DEFAULT_AVERAGE_CORNER_GOALS,
+		const stat& aAverageFarShotGoals = DEFAULT_AVERAGE_FAR_SHOT_GOALS,
+		const stat& a1vs1GKGoalProbability = DEFAULT_1VS1_GK_GOAL_PROBABILITY,
+		const stat& a1vs1DFGoalProbability = DEFAULT_1VS1_DF_GOAL_PROBABILITY,
+		const stat& aExtraCornerProbability = DEFAULT_EXTRA_CORNER_PROBABILITY
+	);
+
 	//! Retrieves the \copybrief mAverageGoals
 	const stat& GetAverageGoals() const noexcept;
 
@@ -43,6 +67,25 @@ public:
 
 	//! Retrieves the \copybrief mExtraCornerProbability
 	const probability& GetExtraCornerProbability() const noexcept;
+
+	//! Default \copybrief mAverageGoals
+	static inline constexpr stat DEFAULT_AVERAGE_GOALS = stat{ 1039 } / 380;
+	//! Default \copybrief mAveragePenaltyGoals
+	static inline constexpr stat DEFAULT_AVERAGE_PENALTY_GOALS = stat{ 74 } / 380;
+	//! Default \copybrief mAverageDirectFreeKickGoals
+	static inline constexpr stat DEFAULT_AVERAGE_DIRECT_FREE_KICK_GOALS = stat{ 17 } / 380;
+	//! Default \copybrief mAverageIndirectFreeKickGoals
+	static inline constexpr stat DEFAULT_AVERAGE_INDIRECT_FREE_KICK_GOALS = stat{ 38 } / 380;
+	//! Default \copybrief mAverageCornerGoals
+	static inline constexpr stat DEFAULT_AVERAGE_CORNER_GOALS = stat{ 151 } / 380;
+	//! Default \copybrief mAverageFarShotGoals
+	static inline constexpr stat DEFAULT_AVERAGE_FAR_SHOT_GOALS = stat{ 147 } / 380;
+	//! Default \copybrief m1vs1GKGoalProbability
+	static inline constexpr stat DEFAULT_1VS1_GK_GOAL_PROBABILITY = 0.35;
+	//! Default \copybrief m1vs1DFGoalProbability
+	static inline constexpr stat DEFAULT_1VS1_DF_GOAL_PROBABILITY = 0.2;
+	//! Default \copybrief mExtraCornerProbability
+	static inline constexpr stat DEFAULT_EXTRA_CORNER_PROBABILITY = 0.25;
 
 private:
 	//! Average number of goals per 90 minutes.
--- src/football/CGoalDrawConfiguration.cpp
+++ src/football/CGoalDrawConfiguration.cpp
@@ -1,7 +1,39 @@
 #include "football/CGoalDrawConfiguration.h"
 
+#include "ExceptionUtils.h"
+#include "NumberUtils.h"
+#include "ProbabilityUtils.h"
+
 namespace futsim::football
 {
+
+CGoalDrawConfiguration::CGoalDrawConfiguration(
+	const stat& aAverageGoals,
+	const stat& aAveragePenaltyGoals,
+	const stat& aAverageDirectFreeKickGoals,
+	const stat& aAverageIndirectFreeKickGoals,
+	const stat& aAverageCornerGoals,
+	const stat& aAverageFarShotGoals,
+	const stat& a1vs1GKGoalProbability,
+	const stat& a1vs1DFGoalProbability,
+	const stat& aExtraCornerProbability
+) try :
+	mAverageGoals( CheckNonNegativeness( aAverageGoals, "average number of goals" ) ),
+	mAveragePenaltyGoals( CheckNonNegativeness( aAveragePenaltyGoals, "average number of penalty goals" ) ),
+	mAverageDirectFreeKickGoals( CheckNonNegativeness( aAverageDirectFreeKickGoals, "average number of direct free kick goals" ) ),
+	mAverageIndirectFreeKickGoals( CheckNonNegativeness( aAverageIndirectFreeKickGoals, "average number of indirect free kick goals" ) ),
+	mAverageCornerGoals( CheckNonNegativeness( aAverageCornerGoals, "average number of corner kick goals" ) ),
+	mAverageFarShotGoals( CheckNonNegativeness( aAverageFarShotGoals, "average number of far shot goals" ) ),
+	m1vs1GKGoalProbability( CheckProbability( a1vs1GKGoalProbability, "probability to score in a 1 on 1 vs GK chance" ) ),
+	m1vs1DFGoalProbability( CheckProbability( a1vs1DFGoalProbability, "probability to score in a 1 on 1 vs DF chance" ) ),
+	mExtraCornerProbability( CheckProbability( aExtraCornerProbability, "probability to be awarded an extra corner" ) )
+{
+	CheckProbability( mExtraCornerProbability + m1vs1GKGoalProbability,
+		"joint probability of getting an extra corner or scoring in a 1 on 1 vs GK chance" );
+	CheckProbability( mExtraCornerProbability + m1vs1DFGoalProbability,
+		"joint probability of getting an extra corner or scoring in a 1 on 1 vs DF chance" );
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the goal draw configuration." )
 
 const CGoalDrawConfiguration::stat& CGoalDrawConfiguration::GetAverageGoals() const noexcept
 {
