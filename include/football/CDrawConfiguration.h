29793a709bd4dc39deb59dd496a774129faac955
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-07 12:40:28 +0100

    Add class members

--- include/football/CDrawConfiguration.h
+++ include/football/CDrawConfiguration.h
@@ -2,6 +2,11 @@
 
 #include "IJsonable.h"
 
+#include "football/CChancesDrawConfiguration.h"
+#include "football/CFoulDrawConfiguration.h"
+#include "football/CGoalDrawConfiguration.h"
+#include "football/CPossessionDrawConfiguration.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +15,28 @@ namespace futsim::football
 */
 class CDrawConfiguration : public IJsonable
 {
+public:
+	//! Retrieves the \copybrief mPossessionDrawConfiguration
+	const CPossessionDrawConfiguration& GetPossessionDrawConfiguration() const noexcept;
+
+	//! Retrieves the \copybrief mFoulDrawConfiguration
+	const CFoulDrawConfiguration& GetFoulDrawConfiguration() const noexcept;
+
+	//! Retrieves the \copybrief mChancesDrawConfiguration
+	const CChancesDrawConfiguration& GetChancesDrawConfiguration() const noexcept;
+
+	//! Retrieves the \copybrief mGoalDrawConfiguration
+	const CGoalDrawConfiguration& GetGoalDrawConfiguration() const noexcept;
+
+private:
+	//! Possession draw configuration.
+	CPossessionDrawConfiguration mPossessionDrawConfiguration;
+	//! Foul draw configuration.
+	CFoulDrawConfiguration mFoulDrawConfiguration;
+	//! Chances draw configuration.
+	CChancesDrawConfiguration mChancesDrawConfiguration;
+	//! Goal draw configuration.
+	CGoalDrawConfiguration mGoalDrawConfiguration;
 };
 
 } // futsim namespace
--- src/football/CDrawConfiguration.cpp
+++ src/football/CDrawConfiguration.cpp
@@ -1,1 +1,26 @@
 #include "football/CDrawConfiguration.h"
+
+namespace futsim::football
+{
+
+const CPossessionDrawConfiguration& CDrawConfiguration::GetPossessionDrawConfiguration() const noexcept
+{
+	return mPossessionDrawConfiguration;
+}
+
+const CFoulDrawConfiguration& CDrawConfiguration::GetFoulDrawConfiguration() const noexcept
+{
+	return mFoulDrawConfiguration;
+}
+
+const CChancesDrawConfiguration& CDrawConfiguration::GetChancesDrawConfiguration() const noexcept
+{
+	return mChancesDrawConfiguration;
+}
+
+const CGoalDrawConfiguration& CDrawConfiguration::GetGoalDrawConfiguration() const noexcept
+{
+	return mGoalDrawConfiguration;
+}
+
+} // futsim::football namespace
