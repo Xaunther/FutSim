bac6c409ee4f7f458c90fd6d7e0484e589fdf1c0
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-12 18:22:05 +0100

    Add member constructor

--- include/football/CTacticsConfiguration.h
+++ include/football/CTacticsConfiguration.h
@@ -18,11 +18,35 @@ protected:
 	using skill_bonus = CTacticConfigurationTypes::skill_bonus;
 
 public:
+	/**
+	 * @brief Member constructor.
+	 * @param aTacticConfigurations \ref mTacticConfigurations
+	 * @param aFavourableTacticSkillBonus \ref mFavourableTacticSkillBonus
+	*/
+	explicit CTacticsConfiguration(
+		const tactic_configurations& aTacticConfigurations = DEFAULT_TACTIC_CONFIGURATIONS,
+		const skill_bonus& aFavourableTacticSkillBonus = DEFAULT_FAVOURABLE_TACTIC_SKILL_BONUS
+	);
+
 	//! Retrieves the \copybrief mTacticConfigurations
 	const tactic_configurations& GetTacticConfigurations() const noexcept;
 
 	//! Retrieves the \copybrief mFavourableTacticSkillBonus
 	const skill_bonus& GetFavourableTacticSkillBonus() const noexcept;
+
+	//! Default \copybrief mTacticConfigurations
+	static inline tactic_configurations DEFAULT_TACTIC_CONFIGURATIONS = {
+		{ "N", CTacticConfiguration{ 0, 0, {"A", "E"} } },
+		{ "A", CTacticConfiguration{ -0.5, -0.2, {"D", "E"} } },
+		{ "D", CTacticConfiguration{ 0.7, -0.2, {"L", "P"} } },
+		{ "L", CTacticConfiguration{ 0.3, -0.6, {"C", "N"} } },
+		{ "C", CTacticConfiguration{ 0.5, -0.8, {"A", "P"} } },
+		{ "P", CTacticConfiguration{ -0.2, 0.6, {"L", "N"} } },
+		{ "E", CTacticConfiguration{ -0.3, 0.2, {"D", "C"} } }
+	};
+
+	//! Default \copybrief mFavourableTacticSkillBonus
+	static inline constexpr skill_bonus DEFAULT_FAVOURABLE_TACTIC_SKILL_BONUS = 1.1;
 
 private:
 	//! Tactic configurations.
--- src/football/CTacticsConfiguration.cpp
+++ src/football/CTacticsConfiguration.cpp
@@ -1,7 +1,32 @@
 #include "football/CTacticsConfiguration.h"
 
+#include "ExceptionUtils.h"
+#include "NumberUtils.h"
+
 namespace futsim::football
 {
+
+namespace
+{
+
+/**
+ * @brief Checks that the tactic configurations are compatible.
+ * @param aTacticConfigurations Tactic configurations.
+*/
+const CTacticsConfigurationTypes::tactic_configurations& CheckTacticConfigurations(
+	const CTacticsConfigurationTypes::tactic_configurations& aTacticConfigurations );
+
+} // anonymous namespace
+
+CTacticsConfiguration::CTacticsConfiguration(
+	const tactic_configurations& aTacticConfigurations,
+	const skill_bonus& aFavourableTacticSkillBonus
+) try :
+	mTacticConfigurations( CheckTacticConfigurations( aTacticConfigurations ) ),
+	mFavourableTacticSkillBonus( CheckNonNegativeness( aFavourableTacticSkillBonus, "favourable tactic skill bonus" ) )
+{
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the tactics configuration." )
 
 const CTacticsConfiguration::tactic_configurations& CTacticsConfiguration::GetTacticConfigurations() const noexcept
 {
@@ -12,5 +37,27 @@ const CTacticsConfiguration::skill_bonus& CTacticsConfiguration::GetFavourableTa
 {
 	return mFavourableTacticSkillBonus;
 }
+
+namespace
+{
+
+const CTacticsConfigurationTypes::tactic_configurations& CheckTacticConfigurations(
+	const CTacticsConfigurationTypes::tactic_configurations& aTacticConfigurations ) try
+{
+	if( aTacticConfigurations.empty() )
+		throw std::invalid_argument{ "The tactic configurations cannot be empty" };
+
+	for( const auto& tacticConfiguration : aTacticConfigurations )
+		for( const auto& favourableTacticID : tacticConfiguration.second.GetFavourableTactics() )
+			if( const auto& favourableTactic = aTacticConfigurations.at( favourableTacticID );
+				favourableTactic.GetFavourableTactics().contains( tacticConfiguration.first ) )
+				throw std::invalid_argument{ "The tactics '" + tacticConfiguration.first + "' and '" + favourableTacticID + "' "
+						"are favourable against each other, which is forbidden." };
+
+	return aTacticConfigurations;
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error checking the tactic configurations." )
+
+} // anonymous namespace
 
 } // futsim::football namespace
