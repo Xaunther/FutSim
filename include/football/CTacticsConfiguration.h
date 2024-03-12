15785c98ac07049c7d0671a47baa4f0dfa994137
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-12 17:43:08 +0100

    Add tactics configuration members

--- include/football/CTacticsConfiguration.h
+++ include/football/CTacticsConfiguration.h
@@ -2,6 +2,9 @@
 
 #include "IJsonable.h"
 
+#include "football/CTacticConfiguration.h"
+#include "football/CTacticsConfigurationTypes.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +13,22 @@ namespace futsim::football
 */
 class CTacticsConfiguration : public IJsonable
 {
+protected:
+	using tactic_configurations = CTacticsConfigurationTypes::tactic_configurations;
+	using skill_bonus = CTacticConfigurationTypes::skill_bonus;
+
+public:
+	//! Retrieves the \copybrief mTacticConfigurations
+	const tactic_configurations& GetTacticConfigurations() const noexcept;
+
+	//! Retrieves the \copybrief mFavourableTacticSkillBonus
+	const skill_bonus& GetFavourableTacticSkillBonus() const noexcept;
+
+private:
+	//! Tactic configurations.
+	tactic_configurations mTacticConfigurations;
+	//! Favourable tactic skill bonus.
+	skill_bonus mFavourableTacticSkillBonus;
 };
 
 } // futsim namespace
--- src/football/CTacticsConfiguration.cpp
+++ src/football/CTacticsConfiguration.cpp
@@ -1,1 +1,16 @@
 #include "football/CTacticsConfiguration.h"
+
+namespace futsim::football
+{
+
+const CTacticsConfiguration::tactic_configurations& CTacticsConfiguration::GetTacticConfigurations() const noexcept
+{
+	return mTacticConfigurations;
+}
+
+const CTacticsConfiguration::skill_bonus& CTacticsConfiguration::GetFavourableTacticSkillBonus() const noexcept
+{
+	return mFavourableTacticSkillBonus;
+}
+
+} // futsim::football namespace
