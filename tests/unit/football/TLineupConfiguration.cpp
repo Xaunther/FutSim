bd314b3c2b985b16c892ca1d668188941fcada5b
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 17:59:22 +0100

    Test outputs

--- tests/unit/football/TLineupConfiguration.cpp
+++ tests/unit/football/TLineupConfiguration.cpp
@@ -65,12 +65,72 @@ void TLineupConfiguration::TestExceptions() const
 std::vector<std::string> TLineupConfiguration::ObtainedResults() const noexcept
 {
 	std::vector<std::string> result;
+	for( const auto& lineupConfiguration : std::initializer_list<CLineupConfiguration>{
+		CLineupConfiguration{},
+		CLineupConfiguration{ { 1, {} }, { 2, 5 }, { 0, 3 } },
+		futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {}
+		} )" ),
+		futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {
+				"Min DFs": 1,
+				"Min MFs": 2,
+				"Max MFs": 5,
+				"Min FWs": 0,
+				"Max FWs": 3
+			}
+		} )" ) } )
+	{
+		result.push_back( std::string{ CLineupConfiguration::JSON_MIN_DFS } + ": " + std::to_string( lineupConfiguration.GetDFRange().first ) );
+		if( lineupConfiguration.GetDFRange().second )
+			result.push_back( std::string{ CLineupConfiguration::JSON_MAX_DFS } + ": " + std::to_string( *lineupConfiguration.GetDFRange().second ) );
+		result.push_back( std::string{ CLineupConfiguration::JSON_MIN_MFS } + ": " + std::to_string( lineupConfiguration.GetMFRange().first ) );
+		if( lineupConfiguration.GetMFRange().second )
+			result.push_back( std::string{ CLineupConfiguration::JSON_MAX_MFS } + ": " + std::to_string( *lineupConfiguration.GetMFRange().second ) );
+		result.push_back( std::string{ CLineupConfiguration::JSON_MIN_FWS } + ": " + std::to_string( lineupConfiguration.GetFWRange().first ) );
+		if( lineupConfiguration.GetFWRange().second )
+			result.push_back( std::string{ CLineupConfiguration::JSON_MAX_FWS } + ": " + std::to_string( *lineupConfiguration.GetFWRange().second ) );
+		futsim::IJsonableTypes::json outputJSON;
+		AddToJSONKey( outputJSON, lineupConfiguration );
+		result.push_back( outputJSON.dump( 1, '\t' ) );
+	}
 	return result;
 }
 
 std::vector<std::string> TLineupConfiguration::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
+	std::vector<std::string> result{
+		"Min DFs: 3",
+		"Max DFs: 6",
+		"Min MFs: 2",
+		"Max MFs: 6",
+		"Min FWs: 0",
+		"Max FWs: 4",
+		"{\n"
+		"	\"Lineup configuration\": {\n"
+		"		\"Min DFs\": 3,\n"
+		"		\"Max DFs\": 6,\n"
+		"		\"Min MFs\": 2,\n"
+		"		\"Max MFs\": 6,\n"
+		"		\"Min FWs\": 0,\n"
+		"		\"Max FWs\": 4\n"
+		"	}\n"
+		"}",
+		"Min DFs: 1",
+		"Min MFs: 2",
+		"Max MFs: 5",
+		"Min FWs: 0",
+		"Max FWs: 3",
+		"{\n"
+		"	\"Lineup configuration\": {\n"
+		"		\"Min DFs\": 1,\n"
+		"		\"Min MFs\": 2,\n"
+		"		\"Max MFs\": 5,\n"
+		"		\"Min FWs\": 0,\n"
+		"		\"Max FWs\": 3\n"
+		"	}\n"
+		"}"
+	};
 	result.reserve( 2 * result.size() );
 	result.insert( result.cend(), result.cbegin(), result.cend() );
 	return result;
