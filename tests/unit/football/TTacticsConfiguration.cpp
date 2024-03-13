2bf0a6815fc9e92705333a6f3ab950d8602aada0
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 08:54:59 +0100

    Test member constructor

--- tests/unit/football/TTacticsConfiguration.cpp
+++ tests/unit/football/TTacticsConfiguration.cpp
@@ -13,6 +13,16 @@ INITIALIZE_TEST( TTacticsConfiguration )
 
 void TTacticsConfiguration::TestExceptions() const
 {
+	// Test member constructor
+	CheckException( []() { CTacticsConfiguration{ CTacticsConfigurationTypes::tactic_configurations{} }; },
+		"The tactic configurations cannot be empty" );
+	CheckException( []() { CTacticsConfiguration{ { { "A", CTacticConfiguration{ 0, 0, { "A" } } } } }; },
+		"The tactics 'A' and 'A' are favourable against each other, which is forbidden." );
+	CheckException( []() { CTacticsConfiguration{ { { "A", CTacticConfiguration{ 0, 0, { "E" } } },
+		{ "E", CTacticConfiguration{ 0, 0, { "A" } } } } }; },
+		"The tactics 'A' and 'E' are favourable against each other, which is forbidden." );
+	CheckException( []() { CTacticsConfiguration{ CTacticsConfiguration::DEFAULT_TACTIC_CONFIGURATIONS, -0.5 }; },
+		"The favourable tactic skill bonus cannot be negative" );
 }
 
 std::vector<std::string> TTacticsConfiguration::ObtainedResults() const noexcept
