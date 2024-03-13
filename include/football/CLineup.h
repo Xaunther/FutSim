8d9bad66ef32211397e8a7f5b43b3e8741563a39
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 10:46:49 +0100

    Add class members

--- include/football/CLineup.h
+++ include/football/CLineup.h
@@ -2,6 +2,8 @@
 
 #include "IJsonable.h"
 
+#include "football/CLineupTypes.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +12,23 @@ namespace futsim::football
 */
 class CLineup : public IJsonable
 {
+protected:
+	using names = CLineupTypes::names;
+	using position_names = CLineupTypes::position_names;
+
+public:
+	/**
+	 * @brief Retrieves the players at a certain position.
+	 * @param aPlayerPosition Position.
+	*/
+	const names& GetPlayers( const E_PLAYER_POSITION& aPlayerPosition ) const noexcept;
+
+	//! Retrieves the substitute players.
+	const names& GetSubs() const noexcept;
+
+private:
+	//! Lined up players by position.
+	position_names mPlayersLineup;
 };
 
 } // futsim namespace
--- src/football/CLineup.cpp
+++ src/football/CLineup.cpp
@@ -1,1 +1,16 @@
 #include "football/CLineup.h"
+
+namespace futsim::football
+{
+
+const CLineup::names& CLineup::GetPlayers( const E_PLAYER_POSITION& aPlayerPosition ) const noexcept
+{
+	return mPlayersLineup[ static_cast< position_names::size_type >( aPlayerPosition ) ];
+}
+
+const CLineup::names& CLineup::GetSubs() const noexcept
+{
+	return mPlayersLineup.back();
+}
+
+} // futsim::football namespace
