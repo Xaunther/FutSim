816edb43cd68f5dd743f622b82d256e2f7393a85
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 17:43:45 +0100

    Test JSON constructor

--- tests/unit/football/TLineupConfiguration.cpp
+++ tests/unit/football/TLineupConfiguration.cpp
@@ -25,6 +25,41 @@ void TLineupConfiguration::TestExceptions() const
 		CLineupConfiguration::DEFAULT_MF_RANGE,
 		CLineupConfigurationTypes::player_count_range{ 1, 0 } }; },
 		"The maximum number of FWs cannot be smaller than the minimum number." );
+
+	// Test JSON constructor
+	CheckException( []() { futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {
+				"Max DFs": 6
+			}
+		} )" ); }, "key 'Min DFs' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {
+				"Min DFs": 1,
+				"Max DFs": 0
+			}
+		} )" ); }, "The maximum number of DFs cannot be smaller than the minimum number." );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {
+				"Max MFs": 6
+			}
+		} )" ); }, "key 'Min MFs' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {
+				"Min MFs": 1,
+				"Max MFs": 0
+			}
+		} )" ); }, "The maximum number of MFs cannot be smaller than the minimum number." );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {
+				"Max FWs": 6
+			}
+		} )" ); }, "key 'Min FWs' not found" );
+	CheckException( []() { futsim::ValueFromJSONKeyString<CLineupConfiguration>( R"( {
+			"Lineup configuration": {
+				"Min FWs": 1,
+				"Max FWs": 0
+			}
+		} )" ); }, "The maximum number of FWs cannot be smaller than the minimum number." );
 }
 
 std::vector<std::string> TLineupConfiguration::ObtainedResults() const noexcept
