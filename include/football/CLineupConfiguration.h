ffbdcac1e03c4cde7cce116c828cb4028fea6e7b
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 16:16:11 +0100

    Add class members

--- include/football/CLineupConfiguration.h
+++ include/football/CLineupConfiguration.h
@@ -2,6 +2,8 @@
 
 #include "IJsonable.h"
 
+#include "football/CLineupConfigurationTypes.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +12,26 @@ namespace futsim::football
 */
 class CLineupConfiguration : public IJsonable
 {
+protected:
+	using player_count_range = CLineupConfigurationTypes::player_count_range;
+
+public:
+	//! Retrieves the \copybrief mDFRange
+	const player_count_range& GetDFRange() const noexcept;
+
+	//! Retrieves the \copybrief mMFRange
+	const player_count_range& GetMFRange() const noexcept;
+
+	//! Retrieves the \copybrief mFWRange
+	const player_count_range& GetFWRange() const noexcept;
+
+private:
+	//! Allowed DFs count range.
+	player_count_range mDFRange;
+	//! Allowed midfielders (DMs+MFs+AMs) count range.
+	player_count_range mMFRange;
+	//! Allowed FWs count range.
+	player_count_range mFWRange;
 };
 
 } // futsim namespace
--- src/football/CLineupConfiguration.cpp
+++ src/football/CLineupConfiguration.cpp
@@ -1,1 +1,21 @@
 #include "football/CLineupConfiguration.h"
+
+namespace futsim::football
+{
+
+const CLineupConfiguration::player_count_range& CLineupConfiguration::GetDFRange() const noexcept
+{
+	return mDFRange;
+}
+
+const CLineupConfiguration::player_count_range& CLineupConfiguration::GetMFRange() const noexcept
+{
+	return mMFRange;
+}
+
+const CLineupConfiguration::player_count_range& CLineupConfiguration::GetFWRange() const noexcept
+{
+	return mFWRange;
+}
+
+} // futsim::football namespace
