049f70ee75a6014e887cf3280914420336540cfd
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 17:21:24 +0100

    Test member constructor

--- tests/unit/football/TLineupConfiguration.cpp
+++ tests/unit/football/TLineupConfiguration.cpp
@@ -13,6 +13,18 @@ INITIALIZE_TEST( TLineupConfiguration )
 
 void TLineupConfiguration::TestExceptions() const
 {
+	// Test member constructor
+	CheckException( []() { CLineupConfiguration{ CLineupConfigurationTypes::player_count_range{ 1, 0 } }; },
+		"The maximum number of DFs cannot be smaller than the minimum number." );
+	// Test member constructor
+	CheckException( []() { CLineupConfiguration{ CLineupConfiguration::DEFAULT_DF_RANGE,
+		CLineupConfigurationTypes::player_count_range{ 1, 0 } }; },
+		"The maximum number of MFs cannot be smaller than the minimum number." );
+	// Test member constructor
+	CheckException( []() { CLineupConfiguration{ CLineupConfiguration::DEFAULT_DF_RANGE,
+		CLineupConfiguration::DEFAULT_MF_RANGE,
+		CLineupConfigurationTypes::player_count_range{ 1, 0 } }; },
+		"The maximum number of FWs cannot be smaller than the minimum number." );
 }
 
 std::vector<std::string> TLineupConfiguration::ObtainedResults() const noexcept
