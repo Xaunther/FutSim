383f9be66c6c01a49984606bacee7773e3fc72fd
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 15:25:44 +0100

    Add class members

--- include/football/CMatchStrategy.h
+++ include/football/CMatchStrategy.h
@@ -2,6 +2,9 @@
 
 #include "IJsonable.h"
 
+#include "football/CLineup.h"
+#include "football/CTacticConfigurationTypes.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +13,21 @@ namespace futsim::football
 */
 class CMatchStrategy : public IJsonable
 {
+protected:
+	using id = CTacticConfigurationTypes::id;
+
+public:
+	//! Retrieves the \copybrief mTacticID
+	const id& GetTacticID() const noexcept;
+
+	//! Retrieves the \copybrief mLineup
+	const CLineup& GetLineup() const noexcept;
+
+private:
+	//! Used tactic ID.
+	id mTacticID;
+	//! Lineup.
+	CLineup mLineup;
 };
 
 } // futsim::football namespace
--- src/football/CMatchStrategy.cpp
+++ src/football/CMatchStrategy.cpp
@@ -1,1 +1,16 @@
 #include "football/CMatchStrategy.h"
+
+namespace futsim::football
+{
+
+const CMatchStrategy::id& CMatchStrategy::GetTacticID() const noexcept
+{
+	return mTacticID;
+}
+
+const CLineup& CMatchStrategy::GetLineup() const noexcept
+{
+	return mLineup;
+}
+
+} // futsim::football namespace
