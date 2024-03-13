8b127faf4d35778a541d73b51b1d1968db9de060
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 15:48:17 +0100

    Test outputs

--- tests/unit/football/TMatchStrategy.cpp
+++ tests/unit/football/TMatchStrategy.cpp
@@ -27,12 +27,42 @@ void TMatchStrategy::TestExceptions() const
 std::vector<std::string> TMatchStrategy::ObtainedResults() const noexcept
 {
 	std::vector<std::string> result;
+	for( const auto& matchStrategy : std::initializer_list<CMatchStrategy>{
+		CMatchStrategy{ "A", CLineup{ CLineupTypes::position_names{ CLineupTypes::names{ "Ter Stegen" } } } },
+		futsim::ValueFromJSONKeyString<CMatchStrategy>( R"( {
+			"Match strategy": {
+				"Tactic": "A",
+				"Lineup": {
+					"GK": [ "Ter Stegen" ]
+				}
+			}
+		} )" ) } )
+	{
+		result.push_back( std::string{ CMatchStrategy::JSON_TACTIC } + ": " + matchStrategy.GetTacticID() );
+		result.push_back( std::string{ CLineup::JSON_KEY } + ": " + matchStrategy.GetLineup().GetPlayers( E_PLAYER_POSITION::GK ).front() );
+		futsim::IJsonableTypes::json outputJSON;
+		AddToJSONKey( outputJSON, matchStrategy );
+		result.push_back( outputJSON.dump( 1, '\t' ) );
+	}
 	return result;
 }
 
 std::vector<std::string> TMatchStrategy::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
+	std::vector<std::string> result{
+		"Tactic: A",
+		"Lineup: Ter Stegen",
+		"{\n"
+		"	\"Match strategy\": {\n"
+		"		\"Tactic\": \"A\",\n"
+		"		\"Lineup\": {\n"
+		"			\"GK\": [\n"
+		"				\"Ter Stegen\"\n"
+		"			]\n"
+		"		}\n"
+		"	}\n"
+		"}"
+	};
 	result.reserve( 2 * result.size() );
 	result.insert( result.cend(), result.cbegin(), result.cend() );
 	return result;
