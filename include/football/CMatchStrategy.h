603d47dbb75919c0abea61b0c412239fbd4b407b
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-13 15:15:09 +0100

    Add game strategy class

---
+++ include/football/CMatchStrategy.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "IJsonable.h"
+
+namespace futsim::football
+{
+
+/**
+ * @brief Class that defines a match strategy
+*/
+class CMatchStrategy : public IJsonable
+{
+};
+
+} // futsim::football namespace
