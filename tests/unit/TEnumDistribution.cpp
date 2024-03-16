7bfc4481a122257eab28cdb4b91669666476801a
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-16 21:05:41 +0100

    Test outputs

--- tests/unit/TEnumDistribution.cpp
+++ tests/unit/TEnumDistribution.cpp
@@ -2,11 +2,31 @@
 
 #include "CEnumDistribution.h"
 
+#include <array>
+#include <functional>
 #include <iostream>
+#include <string>
 
 using namespace futsim;
 
 INITIALIZE_TEST( TEnumDistribution )
+
+enum class myEnum
+{
+	A,
+	B,
+	C
+};
+
+std::string ToString( const myEnum& aEnum ) noexcept
+{
+	switch( aEnum )
+	{
+	case myEnum::A: return "A";
+	case myEnum::B: return "B";
+	default: return "C";
+	}
+}
 
 void TEnumDistribution::TestExceptions() const
 {
@@ -15,13 +35,55 @@ void TEnumDistribution::TestExceptions() const
 std::vector<std::string> TEnumDistribution::ObtainedResults() const noexcept
 {
 	std::vector<std::string> result;
+
+	const std::array<double, 3> weightArray{ 1, 1, 1 };
+
+	for( const auto& enumDistribution : {
+		CEnumDistribution<myEnum>{},
+		CEnumDistribution<myEnum>{ weightArray.cbegin(), weightArray.cend() },
+		CEnumDistribution<myEnum>{ { 1, 2, 3 } },
+		CEnumDistribution<myEnum>{ 3, 0.5, 3.5, std::identity{} },
+		CEnumDistribution<myEnum>{ CEnumDistribution<myEnum>::param_type{} }
+		} )
+	{
+		result.push_back( "min: " + ToString( enumDistribution.min() ) );
+		result.push_back( "max: " + ToString( enumDistribution.max() ) );
+		result.push_back( "Probabilities:" );
+		for( const auto& probability : enumDistribution.probabilities() )
+			result.push_back( std::to_string( probability ) );
+
+	}
 	return result;
 }
 
 std::vector<std::string> TEnumDistribution::ExpectedResults() const noexcept
 {
-	std::vector<std::string> result;
-	result.reserve( 2 * result.size() );
-	result.insert( result.cend(), result.cbegin(), result.cend() );
-	return result;
+	return std::vector<std::string>{
+		"min: A",
+			"max: A",
+			"Probabilities:",
+			"1.000000",
+			"min: A",
+			"max: C",
+			"Probabilities:",
+			"0.333333",
+			"0.333333",
+			"0.333333",
+			"min: A",
+			"max: C",
+			"Probabilities:",
+			"0.166667",
+			"0.333333",
+			"0.500000",
+			"min: A",
+			"max: C",
+			"Probabilities:",
+			"0.166667",
+			"0.333333",
+			"0.500000",
+			"min: A",
+			"max: A",
+			"Probabilities:",
+			"1.000000",
+	};
 }
