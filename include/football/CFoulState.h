9044ba11af423c24f91b666c6901133cf74a633c
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-20 08:25:07 +0100

    Add class members

--- include/football/CFoulState.h
+++ include/football/CFoulState.h
@@ -2,6 +2,9 @@
 
 #include "IJsonable.h"
 
+#include "CPersonTypes.h"
+#include "CFoulDrawConfigurationTypes.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +13,22 @@ namespace futsim::football
 */
 class CFoulState : public IJsonable
 {
+protected:
+	using name_type = CPersonTypes::name_type;
+	using E_FOUL_DRAW_OUTCOME = CFoulDrawConfigurationTypes::E_FOUL_DRAW_OUTCOME;
+
+public:
+	//! Retrieves the \copybrief mCommitter
+	std::string_view GetCommitter() const noexcept;
+
+	//! Retrieves the \copybrief mOutcome
+	const E_FOUL_DRAW_OUTCOME& GetOutcome() const noexcept;
+
+private:
+	//! Player that committed the foul.
+	name_type mCommitter;
+	//! Foul draw outcome.
+	E_FOUL_DRAW_OUTCOME mOutcome;
 };
 
 } // futsim::football namespace
--- src/football/CFoulState.cpp
+++ src/football/CFoulState.cpp
@@ -1,1 +1,16 @@
 #include "football/CFoulState.h"
+
+namespace futsim::football
+{
+
+std::string_view CFoulState::GetCommitter() const noexcept
+{
+	return mCommitter;
+}
+
+const CFoulDrawConfigurationTypes::E_FOUL_DRAW_OUTCOME& CFoulState::GetOutcome() const noexcept
+{
+	return mOutcome;
+}
+
+} // futsim::football namespace
