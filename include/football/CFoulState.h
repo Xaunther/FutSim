648d3c9ff01207f61dc5cb787a2e8adcaf40c94e
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-20 08:17:16 +0100

    Add foul state class

---
+++ include/football/CFoulState.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "IJsonable.h"
+
+namespace futsim::football
+{
+
+/**
+ * @brief Class that represents the state of a foul.
+*/
+class CFoulState : public IJsonable
+{
+};
+
+} // futsim::football namespace
