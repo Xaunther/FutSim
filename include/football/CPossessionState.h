640ddf7bef9258dc58496f3df5599d3fbe7d04ad
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-21 15:45:17 +0100

    Add possession state class

---
+++ include/football/CPossessionState.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "IJsonable.h"
+
+namespace futsim::football
+{
+
+/**
+ * @brief Class that represents the state of a possession draw.
+*/
+class CPossessionState : public IJsonable
+{
+};
+
+} // futsim::football namespace
