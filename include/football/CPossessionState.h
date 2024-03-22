bec4f19863471cc6e12cab94a4d48e7e169d66ea
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-03-22 10:45:37 +0100

    Add constructor

--- include/football/CPossessionState.h
+++ include/football/CPossessionState.h
@@ -4,8 +4,13 @@
 
 #include "football/CPossessionStateTypes.h"
 
+#include "football/CMatch.h"
+#include "football/CMatchConfiguration.h"
+#include "football/CTeamStrategy.h"
 #include "football/CPossessionDrawConfigurationTypes.h"
 #include "CPersonTypes.h"
+
+#include "ExceptionUtils.h"
 
 namespace futsim::football
 {
@@ -20,6 +25,25 @@ protected:
 	using E_POSSESSION_DRAW_OUTCOME = CPossessionDrawConfigurationTypes::E_POSSESSION_DRAW_OUTCOME;
 
 public:
+	/**
+	 * @brief Constructor from the match definition, configuration and current strategies.
+	 * @param aMatch Match definition.
+	 * @param aMatchConfiguration Match configuration.
+	 * @param aAttackingTeamStrategy Current match strategy for the attacking team.
+	 * @param aDefendingTeamStrategy Current match strategy for the defending team.
+	 * @param aHomeTeamAttack Whether the home team is attacking.
+	 * @param aGenerator RNG to use.
+	 * @pre The team strategies must both pass \ref CMatchConfiguration::CheckTeamStrategy and \ref CMatch::CheckTeamStrategy
+	*/
+	explicit CPossessionState(
+		const CMatch& aMatch,
+		const CMatchConfiguration& aMatchConfiguration,
+		const CTeamStrategy& aAttackingTeamStrategy,
+		const CTeamStrategy& aDefendingTeamStrategy,
+		const bool aHomeTeamAttack,
+		std::uniform_random_bit_generator auto& aGenerator
+	);
+
 	//! Retrieves the \copybrief mOutcome
 	const E_POSSESSION_DRAW_OUTCOME& GetOutcome() const noexcept;
 
@@ -37,5 +61,46 @@ private:
 	//! Player that made the pass, if any.
 	optional_name mPasser;
 };
+
+CPossessionState::CPossessionState(
+	const CMatch& aMatch,
+	const CMatchConfiguration& aMatchConfiguration,
+	const CTeamStrategy& aAttackingTeamStrategy,
+	const CTeamStrategy& aDefendingTeamStrategy,
+	const bool aHomeTeamAttack,
+	std::uniform_random_bit_generator auto& aGenerator
+) try
+{
+	using skill_bonus = CTacticConfigurationTypes::skill_bonus;
+
+	// Calculate each player's effective skill
+	std::vector<skill_bonus> attackSkills, defenseSkills;
+	attackSkills.reserve( CLineupConfiguration::MAX_PLAYERS );
+	defenseSkills.reserve( CLineupConfiguration::MAX_PLAYERS );
+
+	aAttackingTeamStrategy.ForEachPlayerSkill( E_PLAYER_SKILL::Ps, aMatch,
+		aMatchConfiguration, aHomeTeamAttack, aDefendingTeamStrategy,
+		[ &attackSkills ]( const auto& aSkill ) { attackSkills.emplace_back( aSkill ); } );
+	aDefendingTeamStrategy.ForEachPlayerSkill( E_PLAYER_SKILL::Tk, aMatch,
+		aMatchConfiguration, !aHomeTeamAttack, aAttackingTeamStrategy,
+		[ &defenseSkills ]( const auto& aSkill ) { defenseSkills.emplace_back( aSkill ); } );
+
+	// Draw possession outcome
+	mOutcome = aMatchConfiguration.GetDrawConfiguration().CreatePossessionDistribution(
+		std::accumulate( defenseSkills.cbegin(), defenseSkills.cend(), skill_bonus{} ),
+		std::accumulate( attackSkills.cbegin(), attackSkills.cend(), skill_bonus{} ) )( aGenerator );
+
+	// Draw acting player
+	const auto DrawPlayer = [ &aGenerator ]( const auto& aTeamStrategy, const auto& aSkills ) {
+		return *( aTeamStrategy.GetLineup().CreatePlayersView<false>()
+			| std::ranges::views::drop( std::discrete_distribution<CLineupTypes::names::size_type>{
+			aSkills.cbegin(), aSkills.cend() }( aGenerator ) ) ).begin(); };
+
+	if( mOutcome == E_POSSESSION_DRAW_OUTCOME::KEEP_POSSESSION )
+		mPasser = DrawPlayer( aAttackingTeamStrategy, attackSkills );
+	else if( mOutcome == E_POSSESSION_DRAW_OUTCOME::COUNTER_ATTACK )
+		mTackler = DrawPlayer( aDefendingTeamStrategy, defenseSkills );
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the possession state." )
 
 } // futsim::football namespace
