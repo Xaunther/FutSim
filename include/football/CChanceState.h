f2e7b4cc9689ca128841848bb43588800e5e630f
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-04-02 17:56:41 +0200

    Add main constructor

--- include/football/CChanceState.h
+++ include/football/CChanceState.h
@@ -5,6 +5,12 @@
 #include "football/types/CChanceState.h"
 #include "football/types/CGoalDrawConfiguration.h"
 
+#include "football/CMatch.h"
+#include "football/CMatchConfiguration.h"
+#include "football/CTeamStrategy.h"
+
+#include "ExceptionUtils.h"
+
 namespace futsim::football
 {
 
@@ -19,6 +25,27 @@ protected:
 	using E_CHANCE_OUTCOME = types::CGoalDrawConfiguration::E_CHANCE_OUTCOME;
 
 public:
+	/**
+	 * @brief Constructor from the match definition, configuration and current strategies.
+	 * @param aMatch Match definition.
+	 * @param aMatchConfiguration Match configuration.
+	 * @param aAttackingTeamStrategy Current match strategy for the attacking team.
+	 * @param aDefendingTeamStrategy Current match strategy for the defending team.
+	 * @param aHomeTeamAttack Whether the home team is attacking.
+	 * @param aIsSetPiece Whether the chance generates from a set piece.
+	 * @param aGenerator RNG to use.
+	 * @pre The team strategies must both pass \ref CMatchConfiguration::CheckTeamStrategy and \ref CMatch::CheckTeamStrategy
+	*/
+	explicit CChanceState(
+		const CMatch& aMatch,
+		const CMatchConfiguration& aMatchConfiguration,
+		const CTeamStrategy& aAttackingTeamStrategy,
+		const CTeamStrategy& aDefendingTeamStrategy,
+		const bool aHomeTeamAttack,
+		const bool aIsSetPiece,
+		std::uniform_random_bit_generator auto& aGenerator
+	);
+
 	//! Retrieves the \copybrief mChanceType
 	const chance_type& GetChanceType() const noexcept;
 
@@ -48,5 +75,132 @@ private:
 	//! Player that made the shot, if any.
 	optional_name mShooter;
 };
+
+namespace detail
+{
+
+/**
+ * @brief Draws a player from a lineup.
+ * @tparam tPlayerSkill Player skill to consider.
+ * @param aGoalDrawConfiguration Goal draw configuration.
+ * @param aLineup Lineup.
+ * @param aGenerator RNG to use.
+ * @return A pair containing the drawn player name and their position.
+*/
+template <E_PLAYER_SKILL tPlayerSkill>
+std::pair<std::string_view, E_PLAYER_POSITION> DrawPlayer( const CGoalDrawConfiguration& aGoalDrawConfiguration,
+	const CLineup& aLineup, std::uniform_random_bit_generator auto& aGenerator );
+
+/**
+ * @brief Draws a chance outcome.
+ * @param aChanceType Chance type to draw an outcome from.
+ * @param aOutcomeDrawer Function that draws the chance outcome.
+*/
+types::CGoalDrawConfiguration::E_CHANCE_OUTCOME DrawOutcome( const types::CChanceState::chance_type& aChanceType, const auto& aOutcomeDrawer );
+
+} // detail namespace
+
+CChanceState::CChanceState(
+	const CMatch& aMatch,
+	const CMatchConfiguration& aMatchConfiguration,
+	const CTeamStrategy& aAttackingTeamStrategy,
+	const CTeamStrategy& aDefendingTeamStrategy,
+	const bool aHomeTeamAttack,
+	const bool aIsSetPiece,
+	std::uniform_random_bit_generator auto& aGenerator
+) try :
+	mChanceType( aIsSetPiece ? chance_type{ aMatchConfiguration.GetDrawConfiguration().CreateSetPieceTypeDistribution()( aGenerator ) } :
+		chance_type{ aMatchConfiguration.GetDrawConfiguration().CreateChanceTypeDistribution()( aGenerator ) } )
+{
+	// Draws an actor for a role and returns their effective skill
+	const auto EffectivePlayerSkillDrawer = [ &aMatch, &aMatchConfiguration, &aGenerator, this ]<E_PLAYER_SKILL tPlayerSkill>
+		( const CTeamStrategy & aOwnTeamStrategy, const CTeamStrategy & aRivalTeamStrategy, const bool aIsHomeTeam )
+	{
+		const auto& drawnPlayer = detail::DrawPlayer<tPlayerSkill>( aMatchConfiguration.GetDrawConfiguration().GetGoalDrawConfiguration(),
+			aOwnTeamStrategy.GetLineup(), aGenerator );
+		// Sets the drawn player to the given role
+		Actor< tPlayerSkill >() = drawnPlayer.first;
+		return CTeamStrategy::CalculateEffectivePlayerSkill( drawnPlayer.first, tPlayerSkill, drawnPlayer.second, aMatch,
+			aMatchConfiguration, aIsHomeTeam, aOwnTeamStrategy.GetTacticID(), aRivalTeamStrategy );
+	};
+
+	// Calls the effective player skill drawer taking into account whether the actor is defending or attacking
+	const auto DrawEffectivePlayerSkill = [ &EffectivePlayerSkillDrawer, &aAttackingTeamStrategy, &aDefendingTeamStrategy, aHomeTeamAttack ]
+		<E_PLAYER_SKILL tPlayerSkill>( )
+	{
+		if constexpr( tPlayerSkill == E_PLAYER_SKILL::St || tPlayerSkill == E_PLAYER_SKILL::Tk )
+			return EffectivePlayerSkillDrawer.template operator() < tPlayerSkill > ( aDefendingTeamStrategy, aAttackingTeamStrategy, !aHomeTeamAttack );
+		else
+			return EffectivePlayerSkillDrawer.template operator() < tPlayerSkill > ( aAttackingTeamStrategy, aDefendingTeamStrategy, aHomeTeamAttack );
+	};
+
+	// Draws an outcome
+	const auto OutcomeDrawer = [ &aMatchConfiguration, &aGenerator, &DrawEffectivePlayerSkill ]<E_PLAYER_SKILL... tSkills>( const auto & aMethod )
+	{
+		return aMethod( aMatchConfiguration.GetDrawConfiguration(), DrawEffectivePlayerSkill.template operator() < tSkills > ( )... )( aGenerator );
+	};
+
+	mOutcome = detail::DrawOutcome( mChanceType, OutcomeDrawer );
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the chance state." )
+
+namespace detail
+{
+
+template <E_PLAYER_SKILL tPlayerSkill>
+std::pair<std::string_view, E_PLAYER_POSITION> DrawPlayer( const CGoalDrawConfiguration& aGoalDrawConfiguration,
+	const CLineup& aLineup, std::uniform_random_bit_generator auto& aGenerator )
+{
+	using enum E_PLAYER_POSITION;
+	while( true )
+	{
+		if( const auto& playerPosition = aGoalDrawConfiguration.CreatePositionDrawDistribution< tPlayerSkill >() ( aGenerator ); playerPosition == GK )
+			return { aLineup.GetPlayers<GK>(), playerPosition };
+		else if( const auto& players = playerPosition == DF ? aLineup.GetPlayers<DF>() :
+			playerPosition == DM ? aLineup.GetPlayers<DM>() :
+			playerPosition == MF ? aLineup.GetPlayers<MF>() :
+			playerPosition == AM ? aLineup.GetPlayers<AM>() :
+			aLineup.GetPlayers<FW>(); players.size() > 0 )
+			return { players[ std::uniform_int_distribution<std::size_t>{ 0, players.size() - 1 }( aGenerator ) ], playerPosition };
+	}
+}
+
+types::CGoalDrawConfiguration::E_CHANCE_OUTCOME DrawOutcome( const types::CChanceState::chance_type& aChanceType, const auto& aOutcomeDrawer )
+{
+	using enum types::CChancesDrawConfiguration::E_SET_PIECE_TYPE;
+	using enum types::CChancesDrawConfiguration::E_CHANCE_TYPE;
+	using enum E_PLAYER_SKILL;
+	if( const auto chance = std::get_if<types::CChancesDrawConfiguration::E_SET_PIECE_TYPE>( &aChanceType ) )
+	{
+		switch( *chance )
+		{
+		case PENALTY:
+			return aOutcomeDrawer.template operator() < St, Sh > ( std::mem_fn( &CDrawConfiguration::CreatePenaltyOutcomeDistribution ) );
+		case DIRECT_FREE_KICK:
+			return aOutcomeDrawer.template operator() < St, Sh > ( std::mem_fn( &CDrawConfiguration::CreateDirectFreeKickOutcomeDistribution ) );
+		case INDIRECT_FREE_KICK:
+			return aOutcomeDrawer.template operator() < St, Tk, Ps, Sh > ( std::mem_fn( &CDrawConfiguration::CreateIndirectFreeKickOutcomeDistribution ) );
+		}
+	}
+	else
+	{
+		switch( std::get<types::CChancesDrawConfiguration::E_CHANCE_TYPE>( aChanceType ) )
+		{
+		case CORNER:
+			return aOutcomeDrawer.template operator() < St, Tk, Ps, Sh > ( std::mem_fn( &CDrawConfiguration::CreateCornerOutcomeDistribution ) );
+		case ONE_ON_ONE_VS_GK:
+			return aOutcomeDrawer.template operator() < St, Ps, Sh > ( std::mem_fn( &CDrawConfiguration::Create1vs1GKOutcomeDistribution ) );
+		case ONE_ON_ONE_VS_DF:
+			return aOutcomeDrawer.template operator() < St, Tk, Ps, Sh > ( std::mem_fn( &CDrawConfiguration::Create1vs1DFOutcomeDistribution ) );
+		case NEAR_SHOT:
+			return aOutcomeDrawer.template operator() < St, Sh > ( std::mem_fn( &CDrawConfiguration::CreateNearShotOutcomeDistribution ) );
+		case FAR_SHOT:
+			return aOutcomeDrawer.template operator() < St, Sh > ( std::mem_fn( &CDrawConfiguration::CreateFarShotOutcomeDistribution ) );
+		}
+	}
+	throw std::domain_error{ "Error drawing the chance outcome." };
+}
+
+} //detail namespace
 
 } // futsim::football namespace
