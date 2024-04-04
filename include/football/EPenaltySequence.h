00c55afa954984bd2f8659628020683a8e799739
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-04-04 11:03:36 +0200

    Define penalty sequence enum through macros

--- include/football/EPenaltySequence.h
+++ include/football/EPenaltySequence.h
@@ -5,15 +5,18 @@
 namespace futsim::football
 {
 
+//! Macro to do something for each penalty sequence.
+#define FOR_EACH_PENALTY_SEQUENCE(DO, ...) \
+	DO(AB, "AB", __VA_ARGS__) \
+	DO(ABBA, "ABBA", __VA_ARGS__) \
+
 //! Enumeration for the penalty sequence.
 enum class E_PENALTY_SEQUENCE {
-	AB,
-	ABBA
+	FOR_EACH_PENALTY_SEQUENCE( FUTSIM_LIST_ENUM )
 };
 
 FUTSIM_SERIALIZE_ENUM( E_PENALTY_SEQUENCE, {
-	{E_PENALTY_SEQUENCE::AB, "AB"},
-	{E_PENALTY_SEQUENCE::ABBA, "ABBA"},
+	FOR_EACH_PENALTY_SEQUENCE( FUTSIM_LIST_ENUM_ID, E_PENALTY_SEQUENCE )
 	} )
 
 } // futsim namespace
