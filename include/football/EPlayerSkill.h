8a00b13523c9389cb4ba025ed8f5272d1ff49e12
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-04-04 11:19:06 +0200

    Define player skill enum through macros

--- include/football/EPlayerSkill.h
+++ include/football/EPlayerSkill.h
@@ -5,19 +5,20 @@
 namespace futsim::football
 {
 
+//! Macro to do something for each player skill.
+#define FOR_EACH_PLAYER_SKILL(DO, ...) \
+    DO(St, "St", __VA_ARGS__) \
+    DO(Tk, "Tk", __VA_ARGS__) \
+    DO(Ps, "Ps", __VA_ARGS__) \
+    DO(Sh, "Sh", __VA_ARGS__) \
+
 //! Enumeration for the different player skills.
 enum class E_PLAYER_SKILL {
-	St,
-	Tk,
-	Ps,
-	Sh,
+	FOR_EACH_PLAYER_SKILL( FUTSIM_LIST_ENUM )
 };
 
 FUTSIM_SERIALIZE_ENUM( E_PLAYER_SKILL, {
-	{E_PLAYER_SKILL::St, "St"},
-	{E_PLAYER_SKILL::Tk, "Tk"},
-	{E_PLAYER_SKILL::Ps, "Ps"},
-	{E_PLAYER_SKILL::Sh, "Sh"},
+	FOR_EACH_PLAYER_SKILL( FUTSIM_LIST_ENUM_ID, E_PLAYER_SKILL )
 	} )
 
 } // futsim::football namespace
