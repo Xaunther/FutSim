140d52e2b527769ed2b3de5a5331359ba5d49d64
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-04-30 09:55:45 +0200

    Add class members

--- include/football/CPlayState.h
+++ include/football/CPlayState.h
@@ -2,6 +2,12 @@
 
 #include "IJsonable.h"
 
+#include "football/types/CPlayState.h"
+
+#include "football/CChanceState.h"
+#include "football/CFoulState.h"
+#include "football/CPossessionState.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +16,27 @@ namespace futsim::football
 */
 class CPlayState : public IJsonable
 {
+protected:
+	using optional_foul_state = types::CPlayState::optional_foul_state;
+	using chances_states = types::CPlayState::chances_states;
+
+public:
+	//! Retrieves the \copybrief mPossessionState
+	const CPossessionState& GetPossessionState() const noexcept;
+
+	//! Retrieves the \copybrief mFoulState
+	const optional_foul_state& GetFoulState() const noexcept;
+
+	//! Retrieves the \copybrief mChancesStates
+	const chances_states& GetChancesStates() const noexcept;
+
+private:
+	//! Possession state.
+	CPossessionState mPossessionState;
+	//! Foul state, if any.
+	optional_foul_state mFoulState;
+	//! Chances states.
+	chances_states mChancesStates;
 };
 
 } // futsim::football namespace
--- src/football/CPlayState.cpp
+++ src/football/CPlayState.cpp
@@ -3,6 +3,19 @@
 namespace futsim::football
 {
 
+const CPossessionState& CPlayState::GetPossessionState() const noexcept
+{
+	return mPossessionState;
+}
 
+const CPlayState::optional_foul_state& CPlayState::GetFoulState() const noexcept
+{
+	return mFoulState;
+}
+
+const CPlayState::chances_states& CPlayState::GetChancesStates() const noexcept
+{
+	return mChancesStates;
+}
 
 } // futsim::football namespace
