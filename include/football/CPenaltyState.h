690abe97557f2393f1efd5841b7d2e358734b478
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-05-03 11:21:07 +0200

    Add main constructor

--- include/football/CPenaltyState.h
+++ include/football/CPenaltyState.h
@@ -2,6 +2,12 @@
 
 #include "football/CChanceState.h"
 
+#include "football/CMatchConfiguration.h"
+#include "football/CPlayer.h"
+#include "football/CTeamStrategy.h"
+
+#include "ExceptionUtils.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +16,41 @@ namespace futsim::football
 */
 class CPenaltyState : private CChanceState
 {
+public:
+	/**
+	 * @brief Constructor from the goalkeeper, penalty taker and configuration.
+	 * @param aMatch Match definition.
+	 * @param aMatchConfiguration Match configuration.
+	 * @param aShooter Penalty taker.
+	 * @param aGoalkeeper Goalkeeper.
+	 * @param aHomeTeamShoot Whether the home team is shooting.
+	 * @param aGenerator RNG to use.
+	*/
+	explicit CPenaltyState(
+		const CMatch& aMatch,
+		const CMatchConfiguration& aMatchConfiguration,
+		const CPlayer& aShooter,
+		const CPlayer& aGoalkeeper,
+		const bool aHomeTeamShoot,
+		std::uniform_random_bit_generator auto& aGenerator
+	);
 };
+
+CPenaltyState::CPenaltyState(
+	const CMatch& aMatch,
+	const CMatchConfiguration& aMatchConfiguration,
+	const CPlayer& aShooter,
+	const CPlayer& aGoalkeeper,
+	const bool aHomeTeamShoot,
+	std::uniform_random_bit_generator auto& aGenerator
+) try :
+	CChanceState( types::CChancesDrawConfiguration::E_SET_PIECE_TYPE::PENALTY,
+		aMatchConfiguration.GetDrawConfiguration().CreatePenaltyOutcomeDistribution(
+			aGoalkeeper.GetPlayerSkills().GetSkill( E_PLAYER_SKILL::St )* CTeamStrategy::CalculateAmbientFactor( aMatch, aMatchConfiguration, !aHomeTeamShoot ),
+			aShooter.GetPlayerSkills().GetSkill( E_PLAYER_SKILL::Sh )* CTeamStrategy::CalculateAmbientFactor( aMatch, aMatchConfiguration, aHomeTeamShoot ) )( aGenerator ),
+		aGoalkeeper.GetKnownName(), {}, {}, aShooter.GetKnownName() )
+{
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the penalty state." )
 
 } // futsim::football namespace
