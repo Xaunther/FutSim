07bb42891fc58ba93dd618f4d6694b3688c2ceb0
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-05-17 09:31:41 +0200

    Test constructor exceptions

--- tests/unit/football/TPenaltyShootoutState.cpp
+++ tests/unit/football/TPenaltyShootoutState.cpp
@@ -10,11 +10,46 @@ using namespace futsim::football;
 using namespace nlohmann;
 
 constexpr bool PRINT_OUTPUT = false;
+namespace
+{
+
+const CTeam TEAM{
+	"Luton Town FC", "lut", "Rob Edwards", {
+	CPlayer{ "Thomas", "Kaminski", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 70, 1, 1, 1 }, {} } },
+	CPlayer{ "Dan", "Potts", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 70, 1, 5 }, {} } },
+	CPlayer{ "Tom", "Lockyer", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 80, 1, 6 }, {} } },
+	CPlayer{ "Issa", "Kaboré", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 60, 1, 7 }, {} } },
+	CPlayer{ "Amari'i", "Bell", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 60, 1, 8 }, {} } },
+	CPlayer{ "Ross", "Barkley", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 1, 80, 9 }, {} } },
+	CPlayer{ "Marvelous", "Nakamba", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 1, 75, 10 }, {} } },
+	CPlayer{ "Pelly Ruddock", "Mpanzu", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 1, 60, 11 }, {} } },
+	CPlayer{ "Jordan", "Clark", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 1, 70, 12 }, {} } },
+	CPlayer{ "Carlton", "Morris", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 1, 1, 75 }, {} } },
+	CPlayer{ "Elijah", "Adebayo", {}, 35, futsim::E_NATIONALITY::ARG, CPlayerSkills{ { 1, 1, 1, 75 }, {} } },
+	}, 1.2, 11000, 2000
+};
+
+const CTeamStrategy HOME_STRATEGY{ "A", CLineup{ "Kaminski",
+	types::CLineup::names{ "Potts", "Lockyer", "Kaboré", "Bell" },
+	{}, types::CLineup::names{ "Barkley", "Nakamba", "Mpanzu", "Clark" }, {},
+	types::CLineup::names{ "Morris", "Adebayo" }, {} } };
+
+const CTeamStrategy AWAY_STRATEGY{ "A", CLineup{ "Kaminski",
+	types::CLineup::names{ "Lockyer", "Kaboré", "Bell" },
+	{}, types::CLineup::names{ "Barkley", "Nakamba", "Mpanzu", "Clark" }, {},
+	types::CLineup::names{ "Morris", "Adebayo" }, {} } };
+
+} // anonymous namespace
 
 INITIALIZE_TEST( TPenaltyShootoutState )
 
 void TPenaltyShootoutState::TestExceptions() const
 {
+	std::mt19937 rng{ 1234 };
+	// Test constructor
+	CheckException( [ & ]() { CPenaltyShootoutState{ CMatch{ TEAM, TEAM, CStadium{ "The New Lawn", 5147, 1 }, "Michael Oliver" },
+			CMatchConfiguration{}, HOME_STRATEGY, AWAY_STRATEGY, true, rng }; },
+		"The match configuration must include a penalty shootout configuration." );
 }
 
 std::vector<std::string> TPenaltyShootoutState::ObtainedResults() const noexcept
