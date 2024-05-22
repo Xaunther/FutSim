e6c185d0fc26c41c94bcb4e59d1eebb4a5e714c6
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-05-22 11:28:58 +0200

    Use policy to construct period state

--- include/football/CPeriodState.h
+++ include/football/CPeriodState.h
@@ -41,15 +41,18 @@ public:
 	 * @param aAwayTeamStrategy Current match strategy for the away team.
 	 * @param aHomeTeamAttack Whether the home team is attacking.
 	 * @param aGenerator RNG to use.
+	 * @param aPeriodPlayPolicy Period play policy.
 	 * @pre The team strategies must both pass \ref CMatchConfiguration::CheckTeamStrategy and \ref CMatch::CheckTeamStrategy
 	*/
+	template <typename T = SDefaultPeriodPlayPolicy> requires types::CPeriodState::period_play_policy<T>
 	explicit CPeriodState(
 		const CMatch& aMatch,
 		const CMatchConfiguration& aMatchConfiguration,
 		const CTeamStrategy& aHomeTeamStrategy,
 		const CTeamStrategy& aAwayTeamStrategy,
 		bool aHomeTeamAttack,
-		std::uniform_random_bit_generator auto& aGenerator
+		std::uniform_random_bit_generator auto& aGenerator,
+		const T& aPeriodPlayPolicy = T{}
 	);
 
 protected:
@@ -96,18 +99,20 @@ private:
 	plays mPlays;
 };
 
+template <typename T> requires types::CPeriodState::period_play_policy<T>
 CPeriodState::CPeriodState(
 	const CMatch& aMatch,
 	const CMatchConfiguration& aMatchConfiguration,
 	const CTeamStrategy& aHomeTeamStrategy,
 	const CTeamStrategy& aAwayTeamStrategy,
 	bool aHomeTeamAttack,
-	std::uniform_random_bit_generator auto& aGenerator
+	std::uniform_random_bit_generator auto& aGenerator,
+	const T& aPeriodPlayPolicy
 )
 {
 	mPlays.reserve( aMatchConfiguration.GetPlayTime().GetPeriodTime() );
 
-	for( futsim::types::CPlayTime::period_time time = 0; time < aMatchConfiguration.GetPlayTime().GetPeriodTime(); ++time )
+	do
 	{
 		if( aHomeTeamAttack )
 			PushPlayState( CPlayState{ aMatch, aMatchConfiguration, aHomeTeamStrategy, aAwayTeamStrategy,
@@ -116,7 +121,7 @@ CPeriodState::CPeriodState(
 			PushPlayState( CPlayState{ aMatch, aMatchConfiguration, aAwayTeamStrategy, aHomeTeamStrategy,
 				aHomeTeamAttack, aGenerator }, aHomeTeamAttack );
 		aHomeTeamAttack = IsHomeTeamAttackNext();
-	}
+	} while( aPeriodPlayPolicy( mPlays, aMatchConfiguration ) );
 }
 
 } // futsim::football namespace
