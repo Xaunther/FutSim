04c4bdfc784eb5904d17c0ce0ff8a35415e30d4f
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-05-22 10:51:58 +0200

    Add extra time period state constructor

--- include/football/CExtraTimePeriodState.h
+++ include/football/CExtraTimePeriodState.h
@@ -22,6 +22,7 @@ public:
 	 * @param aHomeTeamAttack Whether the home team is attacking.
 	 * @param aGenerator RNG to use.
 	 * @pre The team strategies must both pass \ref CMatchConfiguration::CheckTeamStrategy and \ref CMatch::CheckTeamStrategy
+	 * @pre The match configuration must pass \ref CheckMatchConfiguration
 	*/
 	explicit CExtraTimePeriodState(
 		const CMatch& aMatch,
@@ -31,6 +32,42 @@ public:
 		bool aHomeTeamAttack,
 		std::uniform_random_bit_generator auto& aGenerator
 	);
+
+	/**
+	 * @brief Returns whether a goal has been scored in the last play.
+	 * @pre At least one play must have been occurred.
+	*/
+	bool HasGoalBeenScoredLast() const;
+
+	/**
+	 * @brief Checks that the match configuration can produce an extra time period.
+	 * @param aMatchConfiguration Match configuration.
+	*/
+	static const CMatchConfiguration& CheckMatchConfiguration( const CMatchConfiguration& aMatchConfiguration );
 };
+
+CExtraTimePeriodState::CExtraTimePeriodState(
+	const CMatch& aMatch,
+	const CMatchConfiguration& aMatchConfiguration,
+	const CTeamStrategy& aHomeTeamStrategy,
+	const CTeamStrategy& aAwayTeamStrategy,
+	bool aHomeTeamAttack,
+	std::uniform_random_bit_generator auto& aGenerator
+) try :
+	CPeriodState( aMatchConfiguration.GetExtraTime()->GetPeriodTime() )
+{
+	const auto& extraTime = *aMatchConfiguration.GetExtraTime();
+	do
+	{
+		if( aHomeTeamAttack )
+			PushPlayState( CPlayState{ aMatch, aMatchConfiguration, aHomeTeamStrategy, aAwayTeamStrategy,
+				aHomeTeamAttack, aGenerator }, aHomeTeamAttack );
+		else
+			PushPlayState( CPlayState{ aMatch, aMatchConfiguration, aAwayTeamStrategy, aHomeTeamStrategy,
+				aHomeTeamAttack, aGenerator }, aHomeTeamAttack );
+		aHomeTeamAttack = IsHomeTeamAttackNext();
+	} while( GetPlays().size() < extraTime.GetPeriodTime() && ( extraTime.GetGoalRule() != E_GOAL_RULE::GOLDEN_GOAL || !HasGoalBeenScoredLast() ) );
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the extra time period state." )
 
 } // futsim::football namespace
--- src/football/CExtraTimePeriodState.cpp
+++ src/football/CExtraTimePeriodState.cpp
@@ -1,1 +1,20 @@
 #include "football/CExtraTimePeriodState.h"
+
+namespace futsim::football
+{
+
+bool CExtraTimePeriodState::HasGoalBeenScoredLast() const
+{
+	using enum types::CGoalDrawConfiguration::E_CHANCE_OUTCOME;
+	const auto& lastPlay = GetPlays().back();
+	return !lastPlay.state.GetChancesStates().empty() && lastPlay.state.GetChancesStates().back().GetChanceOutcome() == GOAL;
+}
+
+const CMatchConfiguration& CExtraTimePeriodState::CheckMatchConfiguration( const CMatchConfiguration& aMatchConfiguration )
+{
+	if( !aMatchConfiguration.GetExtraTime() )
+		throw std::invalid_argument{ "The match configuration cannot configure an extra time period state." };
+	return aMatchConfiguration;
+}
+
+} // futsim::football namespace
