7c5e0c73ea8715a814036c78e9173fcdaa33bb20
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-06-04 14:14:05 +0200

    Add period policy concept

--- include/football/types/CPeriodStates.h
+++ include/football/types/CPeriodStates.h
@@ -5,6 +5,7 @@
 namespace futsim::football
 {
 
+class CMatchConfiguration;
 class CPeriodState;
 
 namespace types::CPeriodStates
@@ -12,6 +13,13 @@ namespace types::CPeriodStates
 
 //! Container for the periods.
 using period_states = std::vector<football::CPeriodState>;
+
+//! Concept for a period play policy
+template<typename T> concept period_policy =
+	requires ( T aPolicy, const period_states & aPeriodStates, const football::CMatchConfiguration & aMatchConfiguration )
+{
+	{ aPolicy( aPeriodStates, aMatchConfiguration ) } -> std::same_as<bool>;
+};
 
 } // types::CPeriodStates namespace
 
