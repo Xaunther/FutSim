988084086563389a39005f34595e91d865a4e603
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-06-04 11:55:37 +0200

    Add class members

--- include/football/CMatchState.h
+++ include/football/CMatchState.h
@@ -2,6 +2,11 @@
 
 #include "IJsonable.h"
 
+#include "types/CMatchState.h"
+
+#include "football/CPeriodStates.h"
+#include "football/CPenaltyShootoutState.h"
+
 namespace futsim::football
 {
 
@@ -10,6 +15,17 @@ namespace futsim::football
 */
 class CMatchState : public IJsonable
 {
+protected:
+	using optional_period_states = types::CMatchState::optional_period_states;
+	using optional_penalty_shootout_state = types::CMatchState::optional_penalty_shootout_state;
+
+private:
+	//! Mandatory play time of the match.
+	CPeriodStates mMandatoryPlayTimeState;
+	//! Optional extra time state.
+	optional_period_states mExtraTimeState;
+	//! Optional penalty shootout state.
+	optional_penalty_shootout_state mPenaltyShootoutState;
 };
 
 } // futsim::football namespace
