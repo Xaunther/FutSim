2d58b7c6015ea920466935fd21ee88768d12e4a0
Alejandro Alfonso <alex.1993.24@gmail.com>
2024-06-04 14:29:41 +0200

    Add constructor

--- include/football/CPeriodStates.h
+++ include/football/CPeriodStates.h
@@ -34,6 +34,26 @@ public:
 		bool operator()( const period_states& aPeriodStates, const CMatchConfiguration& aMatchConfiguration ) const;
 	};
 
+	/**
+	 * @brief Constructor from the match definition, configuration and current strategies.
+	 * @param aMatch Match definition.
+	 * @param aMatchConfiguration Match configuration.
+	 * @param aHomeTeamStrategy Current match strategy for the home team.
+	 * @param aAwayTeamStrategy Current match strategy for the away team.
+	 * @param aGenerator RNG to use.
+	 * @param aPeriodPolicy Period policy.
+	 * @pre The team strategies must both pass \ref CMatchConfiguration::CheckTeamStrategy and \ref CMatch::CheckTeamStrategy
+	*/
+	template <typename T = SDefaultPeriodPolicy> requires types::CPeriodStates::period_policy<T>
+	explicit CPeriodStates(
+		const CMatch& aMatch,
+		const CMatchConfiguration& aMatchConfiguration,
+		const CTeamStrategy& aHomeTeamStrategy,
+		const CTeamStrategy& aAwayTeamStrategy,
+		std::uniform_random_bit_generator auto& aGenerator,
+		const T& aPeriodPolicy = T{}
+	);
+
 	//! Retrieves the \copybrief mStates
 	const period_states& GetStates() const noexcept;
 
@@ -41,5 +61,29 @@ private:
 	//! Period states.
 	period_states mStates;
 };
+
+template <typename T> requires types::CPeriodStates::period_policy<T>
+CPeriodStates::CPeriodStates(
+	const CMatch& aMatch,
+	const CMatchConfiguration& aMatchConfiguration,
+	const CTeamStrategy& aHomeTeamStrategy,
+	const CTeamStrategy& aAwayTeamStrategy,
+	std::uniform_random_bit_generator auto& aGenerator,
+	const T& aPeriodPolicy
+) try
+{
+	auto homeTeamAttack = std::bernoulli_distribution{}( aGenerator );
+	do
+	{
+		if( homeTeamAttack )
+			mStates.emplace_back( aMatch, aMatchConfiguration, aHomeTeamStrategy, aAwayTeamStrategy,
+				homeTeamAttack, aGenerator );
+		else
+			mStates.emplace_back( aMatch, aMatchConfiguration, aAwayTeamStrategy, aHomeTeamStrategy,
+				homeTeamAttack, aGenerator );
+		homeTeamAttack = !homeTeamAttack;
+	} while( aPeriodPolicy( mStates, aMatchConfiguration ) );
+}
+FUTSIM_CATCH_AND_RETHROW_EXCEPTION( std::invalid_argument, "Error creating the period states." )
 
 } // futsim::football namespace
