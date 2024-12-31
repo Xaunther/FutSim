42def3be541a23c0e963d47a59aef704e81b5585
Xaunder <alex.1993.24@gmail.com>
2024-12-31 13:44:58 +0100

    Improve CPossessionDrawConfiguration traits access

--- include/football/CPossessionDrawConfiguration.h
+++ include/football/CPossessionDrawConfiguration.h
@@ -23,13 +23,21 @@ public:
 	static inline constexpr probability KEEP_POSSESSION_PROBABILITY = probability{ 295952 } / 460939;
 };
 
+template <> struct json_traits<football::CPossessionDrawConfiguration>
+{
+	//! JSON key for the class.
+	static inline constexpr std::string_view KEY = "Possession draw configuration";
+	//! JSON key for the \copybrief football::CPossessionDrawConfiguration::mKeepPossessionProbability
+	static inline constexpr std::string_view KEEP_POSSESSION_PROBABILITY_KEY = "Keep possession probability";
+};
+
 namespace football
 {
 
 /**
  * @brief Class that configures the possession draw.
 */
-class CPossessionDrawConfiguration : public IJsonable
+class CPossessionDrawConfiguration : public IJsonable, protected default_traits<CPossessionDrawConfiguration>, protected json_traits<CPossessionDrawConfiguration>
 {
 protected:
 	using probability = types::CDrawConfiguration::probability;
@@ -40,7 +48,7 @@ public:
 	 * @param aKeepPossessionProbability \ref mKeepPossessionProbability
 	*/
 	explicit CPossessionDrawConfiguration(
-		const probability& aKeepPossessionProbability = default_traits<CPossessionDrawConfiguration>::KEEP_POSSESSION_PROBABILITY );
+		const probability& aKeepPossessionProbability = KEEP_POSSESSION_PROBABILITY );
 
 	/**
 	 * @brief JSON constructor.
@@ -64,13 +72,5 @@ private:
 };
 
 } // football namespace
-
-template <> struct json_traits<football::CPossessionDrawConfiguration>
-{
-	//! JSON key for the class.
-	static inline constexpr std::string_view KEY = "Possession draw configuration";
-	//! JSON key for the \copybrief football::CPossessionDrawConfiguration::mKeepPossessionProbability
-	static inline constexpr std::string_view KEEP_POSSESSION_PROBABILITY = "Keep possession probability";
-};
 
 } // futsim namespace
