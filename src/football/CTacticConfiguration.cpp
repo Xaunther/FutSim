3e4f92b12cb220b5b78b576725cefc0ed5549761
Xaunder <alex.1993.24@gmail.com>
2025-05-02 13:11:24 +0200

    Supress clang-tidy warnings

--- src/football/CTacticConfiguration.cpp
+++ src/football/CTacticConfiguration.cpp
@@ -64,8 +64,10 @@ const CTacticConfiguration::position_penalties& CTacticConfiguration::GetPositio
 const CTacticConfiguration::skill_bonus& CTacticConfiguration::GetSkillBonus(
 	const E_PLAYER_POSITION& aPlayerPosition, const E_PLAYER_SKILL& aPlayerSkill ) const noexcept
 {
+	// NOLINTBEGIN(cppcoreguidelines-pro-bounds-constant-array-index)
 	return mBonusesTable[ static_cast<bonuses_table::size_type>( aPlayerPosition ) ]
-		[ static_cast<bonuses_table::value_type::size_type>( aPlayerSkill ) ]; // NOLINT(cppcoreguidelines-pro-bounds-constant-array-index)
+		[ static_cast<bonuses_table::value_type::size_type>( aPlayerSkill ) ];
+	// NOLINTEND(cppcoreguidelines-pro-bounds-constant-array-index)
 }
 
 CTacticConfiguration::skill_bonus& CTacticConfiguration::SkillBonus(
