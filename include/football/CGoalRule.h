a8ccbd8f94155ba09b562a8374766b5511c8b332
Xaunder <alex.1993.24@gmail.com>
2025-05-27 15:00:30 +0200

    Create nested goal rules

--- include/football/CGoalRule.h
+++ include/football/CGoalRule.h
@@ -3,6 +3,8 @@
 #include "IJsonable.h"
 #include "football/traits/CGoalRule.h"
 
+#include <variant>
+
 namespace futsim::football
 {
 
@@ -11,7 +13,30 @@ namespace futsim::football
 */
 class CGoalRule : public IJsonable, protected json_traits<CGoalRule>
 {
+public:
+	//! Class for the absence of a goal rule.
+	struct NO
+	{
+		//! Conversion to string.
+		inline constexpr operator std::string_view() const noexcept{ return CGoalRule::NO_KEY; }
+	};
 
+	//! Class for a silver goal rule.
+	struct SILVER_GOAL
+	{
+		//! Conversion to string.
+		inline constexpr operator std::string_view() const noexcept{ return CGoalRule::SILVER_GOAL_KEY; }
+	};
+
+	//! Class for a golden goal rule.
+	struct GOLDEN_GOAL
+	{
+		//! Conversion to string.
+		inline constexpr operator std::string_view() const noexcept{ return CGoalRule::GOLDEN_GOAL_KEY; }
+	};
+
+	//! Type for the goal rule variant.
+	using variant = std::variant<NO, SILVER_GOAL, GOLDEN_GOAL>;
 };
 
 } // futsim::football namespace
